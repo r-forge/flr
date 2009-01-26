7742d202d2f2030cf908d901982a6f01e2df9060
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2009-01-26 14:29:55 +0000

    Initial upload of FLCore 2.0 Final RC
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/FLCore/tests/FLFleet.R
@@ -0,0 +1,11 @@
+# FLFleet - «Short one line description»
+# FLFleet
+# FLCore/tests/FLFleet
+
+# Copyright 2003-2008 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira, Cefas
+# $Id: FLFleet.R,v 1.9 2008/12/12 12:39:45 imosqueira Exp $
+
+library(FLCore)
+
+# BUG IM_20081212_1305 validity(FLFleet)
