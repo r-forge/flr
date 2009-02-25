551db41ae3c0fc86dfe8db1cf396f6bdd05c82b9
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2009-02-25 16:37:32 +0000

    Adding DLL registration
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/FLBRP/src/init.c
@@ -0,0 +1,52 @@
+/*
+ * init.c = 
+ *
+ * Author : Iago Mosqueira <imosqueira@suk.azti.es> AZTI Tecnalia 
+ * $Id: $
+ *
+ */
+
+#include <R.h>
+#include <Rdefines.h>
+#include <R_ext/Rdynload.h>
+#include <R_ext/Visibility.h>
+
+#ifdef WIN32
+   #include <windows.h>
+   #define SEXPDLLExport __declspec(dllexport) SEXP __cdecl    
+#else
+   #define SEXPDLLExport SEXP    
+#endif
+
+extern SEXPDLLExport Adolc_gr_tapeless(SEXP xX);
+extern SEXPDLLExport equilibrium(SEXP xbrp, SEXP xSR);
+extern SEXPDLLExport ypr(SEXP xbrp, SEXP xSR);
+extern SEXPDLLExport spr(SEXP xbrp, SEXP xSR);
+extern SEXPDLLExport hcrYield(SEXP xbrp, SEXP xSR, SEXP xFbar);
+extern SEXPDLLExport computeRefpts(SEXP xbrp, SEXP xref, SEXP xSR);
+extern SEXPDLLExport brp(SEXP xbrp, SEXP xref, SEXP xSR);
+
+static const R_CallMethodDef callMethods[] = {
+        {"Adolc_gr_tapeless", (DL_FUNC) &Adolc_gr_tapeless, 1},
+        {"equilibrium", (DL_FUNC) &equilibrium, 2},
+        {"ypr", (DL_FUNC) &ypr, 2},
+        {"spr", (DL_FUNC) &spr, 2},
+        {"hcrYield", (DL_FUNC) &hcrYield, 3},
+        {"computeRefpts", (DL_FUNC) &computeRefpts, 3},
+        {"brp", (DL_FUNC) &brp, 3},
+        {NULL, NULL, 0}
+};
+
+void
+     R_init_foo(DllInfo *info)
+     {
+        R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+        R_useDynamicSymbols(info, FALSE);
+        R_RegisterCCallable("FLBRP", "Adolc_gr_tapeless", (DL_FUNC) Adolc_gr_tapeless);
+        R_RegisterCCallable("FLBRP", "equilibrium", (DL_FUNC) &equilibrium);
+        R_RegisterCCallable("FLBRP", "ypr", (DL_FUNC) &ypr);
+        R_RegisterCCallable("FLBRP", "spr", (DL_FUNC) &spr);
+        R_RegisterCCallable("FLBRP", "hcrYield", (DL_FUNC) &hcrYield);
+        R_RegisterCCallable("FLBRP", "computeRefpts", (DL_FUNC) &computeRefpts);
+        R_RegisterCCallable("FLBRP", "brp", (DL_FUNC) &brp);
+     }
