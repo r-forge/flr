172441438297b3d82e477c6165b927c6993ce74d
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2009-03-04 21:40:17 +0000

    maxfbar in FLBRP@range was named minfbar
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@144 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FLBRP/R/constructors.R
+++ pkg/FLBRP/R/constructors.R
@@ -3,7 +3,7 @@
 
 # Copyright 2003-2009 FLR Team. Distributed under the GPL 2 or later
 # Maintainers: Laurence Kell, Cefas & Santiago Cerviño, IEO
-# Last Change: 04 Mar 2009 19:05
+# Last Change: 04 Mar 2009 22:39
 # $Id$
 
 # FLBRP {{{
@@ -48,7 +48,7 @@ setMethod('FLBRP', signature(object='missing', sr='missing'),
     if(!'range' %in% names(args))
     {
       dims <- dims(slot(res, i))
-      range <- list(min=dims$min, max=dims$max, minfbar=dims$min, minfbar=dims$max,
+      range <- list(min=dims$min, max=dims$max, minfbar=dims$min, maxfbar=dims$max,
         plusgroup=dims$max)
       slot(res, 'range') <- unlist(range)
     }
