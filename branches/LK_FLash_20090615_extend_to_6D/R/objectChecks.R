4e1212161ea2f0a78b11dcba3e3abb32f72f71d0
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2009-06-15 15:05:25 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@247 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/LK_FLash_20090615_extend_to_6D/R/objectChecks.R
@@ -0,0 +1,73 @@
+# Functions and routines to check dimensions of objects - particularly Fleets and Biols
+
+chkDms<-function(biol,flt,yrs){
+#browser()
+    biolDms<-unlist(dims(biol))
+    fltDms <-unlist(dims(flt))
+    #        names(fltDms)[8:11]<-names(biolDms)[8:11]
+    ##dims(bt4[,'sol'])
+
+    jntDms<-c("min","max","minyear","maxyear","unit","season","area","iter")
+
+    if (!all(fltDms[jntDms]==biolDms[jntDms]))
+    return("dims in fleets and biol don't match")
+    if (!all(yrs %in% fltDms["minyear"]:fltDms["maxyear"]))
+    return("Target years and years in fleets and biol don't match")
+
+    yrs<-(min(yrs)-as.integer(fltDms["min"])):max(yrs)
+    if (!all(yrs %in% fltDms["minyear"]:fltDms["maxyear"]))
+    return("Earlier years needed in biol for SSB calculation")
+
+    res<-as.numeric(fltDms[jntDms])
+    names(res)<-names(fltDms[jntDms])
+
+    return(res)}
+
+chkFleets<-function(fleets){
+   nmfleet =names( fleets)
+   nfleet  =length(fleets)
+
+   nmmetier=dims(fleets)$metiers
+   nmetier =length(nmmetier)
+
+   nmspp   =dims(fleets)$catches
+   nspp    =length(nmspp)
+   
+   vl <-c("min","max","minyear","maxyear","unit", "season", "area", "iter")         
+   vls<-c("min","max","minyear","maxyear","units","seasons","areas","iters")         
+   ref<-unlist(dims(fleets))[vls]
+   for (iFlt in nfleet){
+      if (!all(unlist(dims(fleets[[1]]))[vls]==ref)) print("stuff") else print("no stuff") 
+      for (iMet in nmetier){
+         if (!all(unlist(dims(fleets[[1]][[1]]))[vl]==ref)) print("stuff") else print("no stuff")  
+         for (iSpp in nspp){
+            if (!all(unlist(dims(fleets[[1]][[1]][[1]]))[vl]==ref)) print("stuff") else print("no stuff") 
+            }}}
+   }    
+    
+chkTrgtArrayIters <- function(object,trgtArray,sr)
+{
+    if (is(object,'FLlst')) object <- object[[1]]
+    # get iterations from trgtArray, stock, SR parameters and SR residuals
+    its<-sort(unique(c(length(dimnames(trgtArray)$iters), dims(object)$iter, length(dimnames(sr$params[[1]])$iter), length(dimnames(sr$residuals[[1]])$iter))))
+    if (length(its)>2 | (length(its)>1 & its[1]!=1)) stop("Iters not 1 or n")
+    if (length(its)==2 & length(dimnames(trgtArray)$iter == 1)){
+        dmns<-dimnames(trgtArray)
+        dmns$iters<-1:its[2]
+        trgtArray<-array(trgtArray,dim=unlist(lapply(dmns,length)),dimnames=dmns)}
+    return(trgtArray)
+}
+
+# check target quantity is factor and that it is currently implemented
+chkTargetQuantity <- function(target)
+{
+    quantityNms<- c("ssb","biomass","catch","landings","discards","f","z","f.landings","f.discards","effort","costs","revenue","profit","mnsz")
+    if (!is(target[,"quantity"],"factor"))
+        target[,"quantity"]<-factor(target[,"quantity"],quantityNms)
+    if (!all(as.character(target[,"quantity"]) %in% quantityNms))
+        stop("invalid quantity in control target")
+    if (any(as.character(target[,"quantity"]) %in% c("costs","revenue","profit")))
+        stop("fwd not yet implemented for 'costs','revenue' or 'profit'")
+	return(target)
+}
+
---
+++ branches/LK_FLash_20090615_extend_to_6D/tests/TestFwdControl.R
@@ -0,0 +1,35 @@
+library(FLash)
+
+##Target
+res<-fwdControl(data.frame(year=1990,val=1))
+
+x  <-list(val=array(11:20,c(1,10),dimnames=list(1,1:10)))
+res<-fwdControl(data.frame(year=1990),trgtArray=x)
+
+x  <-list(min=array( 1:10,c(1,10),dimnames=list(1,1:10)),
+          max=array(21:30,c(1,10),dimnames=list(1,1:10)))
+res<-fwdControl(data.frame(year=1990),trgtArray=x)
+
+x<-list(min=array( 1:10,c(1,10),dimnames=list(1,1:10)),
+        val=array(11:20,c(1,10),dimnames=list(1,1:10)),
+        max=array(21:30,c(1,10),dimnames=list(1,1:10)))
+res<-fwdControl(data.frame(year=1990),trgtArray=x)
+
+res<-fwdControl(data.frame(year=1990:1991,val=2:3))
+
+x<-list(val=array(1:20,c(2,10),dimnames=list(1:2,1:10)))
+res<-fwdControl(data.frame(year=1990:1991),trgtArray=x)
+
+x<-array(rep(1:10,each=3),dim=c(1,3,10),dimnames=list(row=1,c("min","val","max"),iter=1:10))
+res<-fwdControl(data.frame(year=1990,quantity="ssb"),trgtArray=x)
+res<-fwdControl(data.frame(year=1990:1991),trgtArray=x)
+
+x<-array(rep(c(1,NA),each=2),dim=c(2,2,10),dimnames=list(row=1:2,c("val","max"),iter=1:10))
+res<-fwdControl(data.frame(year=1990:1991),trgtArray=x)
+
+##Target & Effort
+res<-fwdControl(data.frame(year=1990,val=1),effort=data.frame(year=1990,val=1))
+
+x  <-list(val=array(11:20,c(1,10),dimnames=list(1,1:10)))
+res<-fwdControl(data.frame(year=1990),trgtArray=x,effort=data.frame(year=1990),effArray=x)
+
