eba06add94a183b5134ede2da622c3d5761b9392
fscott (Finlay Scott) <drfinlayscott@gmail.com>
2009-07-31 15:00:03 +0000

    made changes to FLSP - added a new more and changed biomass method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@288 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FLAssess/R/FLSP.R
+++ pkg/FLAssess/R/FLSP.R
@@ -17,149 +17,16 @@ setClass('FLSP', representation(
 # FLSP()	{{{
 setGeneric('FLSP', function(model, ...)
 		standardGeneric('FLSP'))
+
 setMethod('FLSP', signature(model='ANY'),
   function(model, ...)
     return(FLModel(model, ..., class='FLSP')))
+
 setMethod('FLSP', signature(model='missing'),
 	function(...)
 		return(FLModel(formula(NULL), ..., class='FLSP'))) # }}}
 
-# PellaTom {{{
-PellaTom <- function(catch, r, K, Q, mpar, delta)
-{
-  dm <- dimnames(catch)
-  catch <- as.vector(catch)
-  biomass <- rep(delta, length(catch))
-  for(y in seq(2, length(catch)))
-    biomass[y] <- biomass[y-1] + r * biomass[y-1] * (1 - biomass[y-1] ^ (mpar-1)) -
-      catch[y-1]/K
-  biomass[biomass <= 0] <- 1e-9
- return(FLQuant(Q*biomass, dimnames=dm))
-}
-
-pellatom <- function()
-{
-  logl <- function(Q, r, K, sigma2, mpar, delta, catch, index)
-  {
-    sum(dnorm(log(index), window(log(PellaTom(catch, r, K, Q, mpar, delta)),
-      start=dims(index)$minyear,end=dims(index)$maxyear), sqrt(sigma2), TRUE), na.rm=TRUE)
-  }
-  initial <- structure(function(catch) return(TRUE),
-    lower=rep(1e-6, 4), upper=rep(Inf, 4))
-  return(list(model=index~PellaTom(catch, r, K, Q, mpar, delta), logl=logl, 
-    initial=initial))
-} # }}}
-
-# PellaTomC {{{
-pellatomC <- function()
-{
-  logl <- function(Q, r, K, sigma2, mpar, delta, catch, index)
-    .Call("loglPellaTomC", c(Q,r,K,sigma2), mpar, delta, catch, index)
-  initial <- structure(function(catch) return(list(Q=70, K=300, r=0.5, sigma2=1)),
-    lower=rep(1e-6, 4), upper=rep(Inf, 4))
-  return(list(model=index ~ .Call('PellatomC', catch, delta, mpar, c(Q, r, K)), 
-    logl=logl, initial=initial))
-} # }}}
-
-# assess  {{{
-setMethod("assess", signature(control="FLSP"),
-   function(control, ...)
-   fmle(control)
-) # }}}
-
-# merge {{{
-setMethod("merge", signature(x="FLStock", y="FLSP"),
-  function(x, y, ...)
-  {
-    quant <- quant(stock(x))
-    dnx <- dimnames(stock(x))
-    dny <- dimnames(y@stock.n)
-
-    # check dimensions match
-    if(!all.equal(dnx[-2], dny[-2]))
-      stop("Mismatch in dimensions: only year can differ between stock and assess")
-
-    # same plusgroup
-    if(x@range['plusgroup'] != x@range['plusgroup'])
-      stop("Mismatch in plusgroup: x and y differ")
-
-    # year ranges match?
-    if(!all(dny[['year']] %in% dnx[['year']]))
-    {
-      years <- as.numeric(unique(c(dnx$year, dny$year)))
-      x <- window(x, start=min(years), end=max(years))
-      y <- window(y, start=min(years), end=max(years))
-    }
-    
-    x <- transform(x, desc=paste(x@desc, "+ FLAssess:", y@name),
-      stock.n=y@stock.n, harvest=y@harvest)
-    x <- transform(x, range=c(unlist(dims(x)[c('min', 'max', 'plusgroup',
-      'minyear', 'maxyear')]), x@range[c('minfbar', 'maxfbar')]))
-        
-    return(x)
-  }
-)   # }}}
-
-# "+"      {{{
-#setMethod("+", signature(e1="FLStock", e2="FLAssess"),
-#	function(e1, e2) {
-#    if(validObject(e1) & validObject(e2))
-#      return(merge(e1, e2))
-#    else
-#      stop("Input objects are not valid: validObject == FALSE")
-#    }
-#)
-#setMethod("+", signature(e1="FLAssess", e2="FLStock"),
-#	function(e1, e2) {
-#    if(validObject(e1) & validObject(e2))
-#      return(merge(e2, e1))
-#    else
-#      stop("Input objects are not valid: validObject == FALSE")
-#    }
-#)   # }}}
-
-# biomass {{{
-if (!isGeneric("biomass"))
-	setGeneric("biomass", function(object, ...)
-    	standardGeneric("biomass"))
-setMethod('biomass', signature(object='FLSP'),
-  function(object) {
-    return(fitted(object) / as.numeric(params(object)['Q',]))})
-# }}}
-
-# harvest rate {{{
-if (!isGeneric("harvest.rate"))
-	setGeneric("harvest.rate", function(object, ...)
-    	standardGeneric("harvest.rate"))
-setMethod('harvest.rate', signature(object='FLSP'),
-  function(object)
-    return(catch(object) / biomass(object)))
-# }}}
-
-# Schaefer {{{
-Schaefer <- function(catch, r, K, q)
-{
-  dm <- dimnames(catch)
-  catch <- as.vector(catch)
-  biomass <- rep(K, length(catch))
-  for(y in seq(2, length(catch)))
-    biomass[y] <- biomass[y-1] + r * biomass[y-1] * (1 - (1/K) * biomass[y-1] ) - catch[y]
-  biomass[biomass <= 0] <- 1e-9
-  print(q)
-  return(FLQuant(q*biomass, dimnames=dm))
-}
-
-
-schaefer <- function()
-{
-  logl <- function(r, K, q, sigma2, catch, index)
-  {
-   sum(dnorm(log(index), window(log(Schaefer(catch, r, K, q)),
-      start=dims(index)$minyear,end=dims(index)$maxyear), sqrt(sigma2), TRUE), na.rm=TRUE)
-  }
-  return(list(model=index~Schaefer(catch, r, K, q), logl=logl))
-} # }}}
-
+# ************** Accessors **************************************
 # Accesors  {{{
 setMethod('catch', signature(object='FLSP'),
   function(object)
@@ -186,7 +53,103 @@ setMethod('index<-', signature(object='FLSP', value='FLQuant'),
 )
 # }}}
 
-# plot  {{{
+# ***********	PellaTom with estimated Q ************
+# PellaTom {{{
+PellaTom <- function(catch, r, K, Q, mpar, delta)
+{
+  dm <- dimnames(catch)
+  catch <- as.vector(catch)
+  biomass <- rep(delta, length(catch))
+  for(y in seq(2, length(catch)))
+    biomass[y] <- biomass[y-1] + (r / (mpar-1)) * biomass[y-1] * (1 - (biomass[y-1] / K) ^ (mpar-1)) - catch[y-1]
+  biomass[biomass <= 0] <- 1e-9
+  #return(list(indexhat=FLQuant(Q*biomass, dimnames=dm), biomass=FLQuant(biomass,dimnames=dm)))
+  return(FLQuant(Q*biomass, dimnames=dm))
+}
+
+pellatom <- function()
+{
+  logl <- function(Q, r, K, sigma2, mpar, delta, catch, index)
+  {
+    sum(dnorm(log(index), window(log(PellaTom(catch, r, K, Q, mpar, delta)),
+      start=dims(index)$minyear,end=dims(index)$maxyear), sqrt(sigma2), TRUE), na.rm=TRUE)
+  }
+  initial <- structure(function(catch) return(TRUE),
+    lower=rep(1e-6, 4), upper=rep(Inf, 4))
+  return(list(model=index~PellaTom(catch, r, K, Q, mpar, delta), logl=logl, 
+    initial=initial))
+} # }}}
+
+# ************ PellaTom with calculated  Q***************
+
+PellaTom_calcQ <- function(catch, index, r, K, mpar, delta)
+{
+    #browser()
+  dm <- dimnames(catch)
+  catch <- as.vector(catch)
+  index <- as.vector(index)
+  biomass <- rep(delta, length(catch))
+  for(y in seq(2, length(catch)))
+    biomass[y] <- biomass[y-1] + (r / (mpar-1)) * biomass[y-1] * (1 - (biomass[y-1] / K) ^ (mpar-1)) - catch[y-1]
+  biomass[biomass <= 0] <- 1e-9
+  
+  mnbio <- (biomass[-length(biomass)] + biomass[-1]) / 2
+  q <- sum(mnbio*index[-length(biomass)])/sum(mnbio*mnbio)
+  # Basically saying that index in year y measures mean of biomass y and y+1
+  indexhat <- FLQuant(q*mnbio,dimnames=dm)
+  # Need to set final year to NA as we have effectively lost last year of index
+  indexhat[,dm$year[length(dm$year)]] <- NA
+  return(indexhat)
+}
+
+#PellaTom_noQ(alb.sp.nq@catch, alb.sp.nq@index, 0.5,200,1,2,250)
+# Need to pass in index to the PellaTom function
+pellatom_calcQ <- function()
+{
+  logl <- function(r, K, sigma2, mpar, delta, catch, index)
+  {
+  # Need to lose final year of index due to q calc 
+    sum(dnorm(log(index[,-ac(dims(index)$maxyear)]), window(log(PellaTom_calcQ(catch, index, r, K, mpar, delta)),
+      start=dims(index)$minyear,end=dims(index)$maxyear-1), sqrt(sigma2), TRUE), na.rm=TRUE)
+  }
+  initial <- structure(function(catch) return(TRUE),
+    lower=rep(1e-6, 4), upper=rep(Inf, 4))
+  return(list(model=index~PellaTom_calcQ(catch, index, r, K, mpar, delta), logl=logl,
+    initial=initial))
+} # }}}
+
+#***************** Methods **********************************
+
+# biomass {{{
+if (!isGeneric("biomass"))
+	setGeneric("biomass", function(object, ...)
+    	standardGeneric("biomass"))
+
+    # Just calculates and returns the biomass
+setMethod('biomass', signature(object='FLSP'),
+  function(object) {
+      #browser()
+    dm <- dimnames(object@catch)
+    biomass <- FLQuant(object@delta,dimnames=dm)
+    # Mixing Quants and Pars is fairly ugly
+    for(y in 2:length(dm$year))
+	# Pretty ugly with either as.numeric or sweeps
+	#biomass[,y] <- biomass[,y-1] + as.numeric(object@params["r",] / (object@mpar-1)) * biomass[,y-1] * (1 - (biomass[,y-1] / as.numeric(object@params["K",])) ^ (object@mpar-1)) - object@catch[,y-1]
+	biomass[,y]<-biomass[,y-1] + sweep(biomass[,y-1],6,object@params["r",]/(object@mpar-1),"*") * (1 - sweep(biomass[,y-1], 6, object@params["K",],"/")^(object@mpar-1)) - object@catch[,y-1]
+    biomass[biomass <= 0] <- 1e-9
+    return(biomass)})
+    # }}}
+
+# harvest rate {{{
+if (!isGeneric("harvest.rate"))
+	setGeneric("harvest.rate", function(object, ...)
+    	standardGeneric("harvest.rate"))
+setMethod('harvest.rate', signature(object='FLSP'),
+  function(object)
+    return(catch(object) / biomass(object)))
+# }}}
+
+# plot {{{
 setMethod('plot', signature(x='FLSP', y='missing'),
   function(x, y, ...)
   {
@@ -241,3 +204,87 @@ setMethod('plot', signature(x='FLSP', y='missing'),
 
   }
 ) # }}}
+
+# assess  {{{
+setMethod("assess", signature(control="FLSP"),
+   function(control, ...)
+   fmle(control)
+) # }}}
+
+# merge {{{
+setMethod("merge", signature(x="FLStock", y="FLSP"),
+  function(x, y, ...)
+  {
+    quant <- quant(stock(x))
+    dnx <- dimnames(stock(x))
+    dny <- dimnames(y@stock.n)
+
+    # check dimensions match
+    if(!all.equal(dnx[-2], dny[-2]))
+      stop("Mismatch in dimensions: only year can differ between stock and assess")
+
+    # same plusgroup
+    if(x@range['plusgroup'] != x@range['plusgroup'])
+      stop("Mismatch in plusgroup: x and y differ")
+
+    # year ranges match?
+    if(!all(dny[['year']] %in% dnx[['year']]))
+    {
+      years <- as.numeric(unique(c(dnx$year, dny$year)))
+      x <- window(x, start=min(years), end=max(years))
+      y <- window(y, start=min(years), end=max(years))
+    }
+    
+    x <- transform(x, desc=paste(x@desc, "+ FLAssess:", y@name),
+      stock.n=y@stock.n, harvest=y@harvest)
+    x <- transform(x, range=c(unlist(dims(x)[c('min', 'max', 'plusgroup',
+      'minyear', 'maxyear')]), x@range[c('minfbar', 'maxfbar')]))
+        
+    return(x)
+  }
+)   # }}}
+#
+## "+"      {{{
+##setMethod("+", signature(e1="FLStock", e2="FLAssess"),
+##	function(e1, e2) {
+##    if(validObject(e1) & validObject(e2))
+##      return(merge(e1, e2))
+##    else
+##      stop("Input objects are not valid: validObject == FALSE")
+##    }
+##)
+##setMethod("+", signature(e1="FLAssess", e2="FLStock"),
+##	function(e1, e2) {
+##    if(validObject(e1) & validObject(e2))
+##      return(merge(e2, e1))
+##    else
+##      stop("Input objects are not valid: validObject == FALSE")
+##    }
+##)   # }}}
+
+# }}}
+
+# Schaefer {{{
+#Schaefer <- function(catch, r, K, q)
+#{
+#  dm <- dimnames(catch)
+#  catch <- as.vector(catch)
+#  biomass <- rep(K, length(catch))
+#  for(y in seq(2, length(catch)))
+#    biomass[y] <- biomass[y-1] + r * biomass[y-1] * (1 - (1/K) * biomass[y-1] ) - catch[y]
+#  biomass[biomass <= 0] <- 1e-9
+#  print(q)
+#  return(FLQuant(q*biomass, dimnames=dm))
+#}
+#
+#
+#schaefer <- function()
+#{
+#  logl <- function(r, K, q, sigma2, catch, index)
+#  {
+#   sum(dnorm(log(index), window(log(Schaefer(catch, r, K, q)),
+#      start=dims(index)$minyear,end=dims(index)$maxyear), sqrt(sigma2), TRUE), na.rm=TRUE)
+#  }
+#  return(list(model=index~Schaefer(catch, r, K, q), logl=logl))
+#} # }}}
+#
