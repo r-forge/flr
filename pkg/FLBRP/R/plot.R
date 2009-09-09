ffb5748e9120f156b17fada0ae25f046679fb29e
lauriekell (Dr ) <laurieKell@gmail.com>
2009-09-09 13:17:17 +0000

    added tapas & rasta plots
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@317 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FLBRP/R/plot.R
+++ pkg/FLBRP/R/plot.R
@@ -272,3 +272,126 @@ plot.r.s<-function(x,ylim,xlim,cols,refpts,obs,ts)
 
          invisible()
 	      }
+
+setMethod("plot", signature(x="FLBRP", y="FLStock"),
+    function(x,y,type="tapas",title="",biCol=c("grey50","grey75"),xlab=expression(SSB:B[MSY]),ylab=expression(F:F[MSY]),maxX=NULL,maxY=NULL,year,axs=TRUE,start=NULL){
+
+    if (type=="tapas")
+       plotTapasFLBRPFLStock(x,y,title,biCol,xlab,ylab,maxX,maxY,axs)
+    else if (type=="rasta")
+       plotRastaFLBRPFLStock(x,y,start)
+    else stop("type has to be ´rasta´or ´tapas´")
+    })
+
+
+## Bivariate CI
+bivariateOrder<-function(dt)
+    {
+    mn      <-apply(dt,2,mean)
+    dt[,1]  <-dt[,1]-mn[1]
+    dt[,2]  <-dt[,2]-mn[2]
+    t.      <-var(dt)
+    cholesky<-chol(t.)
+    dt      <-as.matrix(dt)%*%ginv(cholesky)
+    dist    <-dt[,1]^2+dt[,2]^2
+
+    return(order(dist))
+    }
+
+plotBeer=function(v){
+  	usr=par("usr")
+  	ymin=usr[3]
+  	ymax=usr[4]
+  	yrng=ymax-ymin
+  	ymin1=usr[3]+.1*yrng
+  	ymax1=usr[4]-.2*yrng
+
+  	yrng1=ymax1-ymin1
+  	ymax2=ymin1+abs(v)*yrng1
+  	xmid=(usr[2]+usr[1])/2
+  	ymid=(ymax1+ymin1)/2
+  	xrng=(usr[2]-usr[1])
+  	xpoly=c(xmid-.15*xrng,xmid-(.15+abs(v)*.1)*xrng,xmid+(.15+abs(v)*.1)*xrng,xmid+.15*xrng,xmid-.15*xrng)
+  	ypoly=c(ymin1,ymax2,ymax2,ymin1,ymin1)
+  	polygon(xpoly,ypoly,col="gold",border="burlywood")
+  	bubblex=runif(round(500*abs(v),0),xmid-(.15+abs(v*.95)*.1)*xrng,xmid+(.15+abs(v*.95)*.1)*xrng)
+  	bubbley=runif(round(500*abs(v),0),ymax2-.02*yrng1,ymax2+.02*yrng1)
+  	points(bubblex,bubbley,pch=21,col = "gold", bg = "white",cex=seq(0.1,1,length=10))
+  	points(c(xmid-.15*xrng,xmid+.15*xrng),c(ymin1,ymin1),type="l",lwd=4)
+  	points(c(xmid-.15*xrng,xmid-.25*xrng),c(ymin1,ymax1),type="l",lwd=4)
+  	points(c(xmid+.15*xrng,xmid+.25*xrng),c(ymin1,ymax1),type="l",lwd=4)
+  	if(v<0){
+  		text(xmid,ymid,labels=c(paste("-",abs(v))),cex=.5+abs(v)*2)
+  	}else{
+  		text(xmid,ymid,labels=c(paste("+",abs(v))),cex=.5+abs(v)*2)
+      }
+    }
+
+plotTapasFLBRPFLStock<-function(x,y,title="",biCol=c("grey50","grey75"),xlab=expression(SSB:B[MSY]),ylab=expression(F:F[MSY]),maxX=NULL,maxY=NULL,year,axs=TRUE)
+    {
+    x     <-refpts(x)["msy",,]
+    y     <-window(y,start=range(y,"minyear"),end=range(y,"maxyear"))
+
+    ssbPts<-ssb( y)/c(x[,"ssb",    ])
+    fbrPts<-fbar(y)/c(x[,"harvest",])
+
+    ssbTrk<-quantile(ssbPts,0.5)
+    fbrTrk<-quantile(fbrPts,0.5)
+
+    ssbPts<-ssbPts[,ac(range(y,"maxyear"))]
+    fbrPts<-fbrPts[,ac(range(y,"maxyear"))]
+
+    plotTapas2(ssbTrk,fbrTrk,ssbPts,fbrPts,title,biCol,xlab,ylab,maxX,maxY,year,axs)
+    }
+
+plotTapas2<-function(ssbTrk,fbrTrk,ssbPts,fbrPts,title="",biCol=c("grey50","grey75"),xlab=expression(SSB:B[MSY]),ylab=expression(F:F[MSY]),maxX=NULL,maxY=NULL,year,axs=TRUE)
+    {
+    fish.pg<-function(maxX,maxY)
+        {
+        polygon(x=c(-0.5,1,1,-0.5),            y=c(1.0,1.0,maxY+.5,maxY+.5),    col="red2")
+        polygon(x=c(1.0,maxX+0.5,maxX+0.5,1.0),y=c(-0.5,-0.5,1.0,1.0),          col="lightgreen")
+        polygon(x=c(-0.5,1,1,-0.5),            y=c(-0.5,-0.5,1.0,1.0),          col="lightgoldenrod1")
+        polygon(x=c(1.0,maxX+0.5,maxX+0.5,1.0),y=c(1.0,1.0,maxY+.5,maxY+.5),    col="lightgoldenrod1")
+        }
+
+    maxX.<-max(ssbTrk,ssbPts,na.rm=T)
+    if (is.null(maxX))
+       maxX<-maxX.
+
+    maxY.<-max(fbrTrk,fbrPts,na.rm=T)
+    if (is.null(maxY))
+       maxY<-maxX.
+
+    plot(fbrTrk~ssbTrk, col="cyan", type="l", xlim=c(0,maxX),ylim=c(0,maxY),xlab=xlab,ylab=ylab,main=paste(title),axes=axs)
+    fish.pg(maxX,maxY)
+    abline(h=1.0,v=1,col="grey")
+
+    t.  <-bivariateOrder(cbind(fbrPts,ssbPts))
+    col.<-rep(biCol,each=as.integer(length(t.)/length(biCol)))
+    points(c(fbrPts)[t.]~c(ssbPts)[t.], col=col.,pch=19,cex=0.75)
+    lines( fbrTrk~ssbTrk, col="grey50",lwd=2)
+    }
+
+plotRastaFLBRPFLStock<-function(x,y,start=NULL,title=""){
+    x<-refpts(x)["msy",,]
+
+    if (!is.null(start))  y<-window(y,start=start)
+
+    ssbTrk<-ssb( y)/c(x[,"ssb",    ])
+    fbrTrk<-fbar(y)/c(x[,"harvest",])
+
+    plotRasta2(ssbTrk,fbrTrk,title)
+    }
+
+plotRasta2<-function(ssbTrk,fbrTrk,title){
+    red   <-FLQuant(0,dimnames=dimnames(ssbTrk))
+    green <-red
+    yellow<-red
+
+    red[   ssbTrk< 1 & fbrTrk> 1]<-1
+    green[ ssbTrk>=1 & fbrTrk<=1]<-1
+    yellow[red   ==0 & green ==0]<-1
+
+    print(xyplot(data~year,groups=qname,data=lapply(FLQuants(green=green,red=red,yellow=yellow),function(x) apply(x,2,mean)),
+               col=c("green","red","yellow"),lwd=3,type="l",xlab="Year",ylab="Probability of being in the Zone"))
+    }
