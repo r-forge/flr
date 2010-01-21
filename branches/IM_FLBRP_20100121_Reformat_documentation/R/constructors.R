bb1311d065255ee285a99910a1905aa3649d33d2
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2010-01-21 15:49:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@411 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/IM_FLBRP_20100121_Reformat_documentation/R/constructors.R
@@ -0,0 +1,190 @@
+# constructors - constructor methods for FLBRP
+# FLBRP/R/constructors.R
+
+# Copyright 2003-2009 FLR Team. Distributed under the GPL 2 or later
+# Maintainers: Laurence Kell, Cefas & Santiago Cerviño, IEO
+# $Id$
+
+# FLBRP
+setGeneric('FLBRP', function(object, sr, ...)
+  standardGeneric('FLBRP'))
+
+# FLBRP(object='missing', sr='missing') {{{
+setMethod('FLBRP', signature(object='missing', sr='missing'),
+  function(..., model=formula(rec~a), params=FLPar(1, params='a'),
+    fbar=FLQuant(seq(0, 4, 0.04)))
+  {
+    args <- list(...)
+
+    res <- do.call(new, c(list(Class='FLBRP', model=model, params=params, fbar=fbar),
+      args))
+    # resize: years
+    slots <- c('fbar.obs', 'landings.obs', 'discards.obs', 'rec.obs', 'ssb.obs',
+      'profit.obs')
+    # find slots not provided as argument
+    empty <- !slots %in% names(args)
+    # if any of them given, use for sizing
+    if(any(empty == FALSE))
+      for(i in slots[empty])
+        slot(res, i) <- FLQuant(dimnames=dimnames(slot(res, slots[!empty][1])))
+
+    # warn: slots to dissapear
+    # c('stock.n', 'landings.n', 'discards.n', 'harvest')
+
+    # resize: ages
+    slots <- c('landings.sel', 'discards.sel', 'bycatch.harvest', 'stock.wt',
+      'landings.wt', 'discards.wt', 'bycatch.wt', 'm', 'mat', 'harvest.spwn', 'm.spwn',
+      'availability', 'price')
+    # find slots not provided as argument
+    empty <- !slots %in% names(args)
+    # if any of them given, use for sizing
+    if(any(empty == FALSE))
+      for(i in slots[empty])
+        slot(res, i) <- FLQuant(dimnames=dimnames(slot(res, slots[!empty][1])))
+
+    # range
+    if(!'range' %in% names(args))
+    {
+      if(exists('i'))
+      {
+        dims <- dims(slot(res, i))
+        range <- list(min=dims$min, max=dims$max, minfbar=dims$min, maxfbar=dims$max,
+          plusgroup=dims$max)
+        slot(res, 'range') <- unlist(range)
+      }
+    }
+  
+    # resize: cost
+    slots <- c('vcost', 'fcost')
+    # find slots not provided as argument
+    empty <- !slots %in% names(args)
+    # if any of them given, use for sizing
+    if(any(empty == FALSE))
+      for(i in slots[empty])
+        slot(res, i) <- FLQuant(dimnames=dimnames(slot(res, slots[!empty][1])))
+
+    if (!("discards.wt"     %in% names(args))) discards.wt    (res)[]<-0
+    if (!("discards.sel"    %in% names(args))) discards.sel   (res)[]<-0
+    if (!("bycatch.harvest" %in% names(args))) bycatch.harvest(res)[]<-0
+    if (!("bycatch.wt"      %in% names(args))) bycatch.wt     (res)[]<-0
+    if (!("availability"    %in% names(args))) availability   (res)[]<-1
+
+    return(res)
+
+  }
+) # }}}
+
+# FLBRP(object='missing', sr='FLSR')  {{{
+setMethod('FLBRP', signature(object='missing', sr='FLSR'),
+  function(sr, ...)
+  {
+    args <- list(...)
+
+    do.call('FLBRP', c(list(model=model(sr), params=params(sr)), args))
+  }
+) # }}}
+
+# FLBRP(object=FLStock, sr=FLSR)  {{{
+setMethod('FLBRP', signature(object='FLStock', sr='FLSR'),
+  function(object, sr, ...)
+  {
+    FLBRP(object=object, model=sr@model, params=sr@params, ...)
+  }
+) # }}}
+
+# FLBRP(object=FLStock, sr=missing) {{{
+setMethod('FLBRP', signature(object='FLStock', sr='missing'),
+  function(object, model=formula(rec~a), params=FLPar(1, params='a'),
+    fbar=seq(0, 4, 0.04), nyears=3, biol.nyears=nyears, fbar.nyears=nyears, sel.nyears=fbar.nyears,
+    na.rm=TRUE, mean='arithmetic', ...)
+    {
+    # dims & dimnames
+    dims <- dims(object)
+    if (!all(c("minfbar","maxfbar") %in% names(range(object))))
+       stop("'minfbar' and 'maxfbar' missing from range")
+    
+    maxyear <- dims$maxyear
+    byears <- ac(seq(maxyear-biol.nyears+1, maxyear))
+    fyears <- ac(seq(maxyear-fbar.nyears+1, maxyear))
+    syears <- ac(seq(maxyear-sel.nyears+1, maxyear))
+    fages <- ac(seq(object@range['minfbar'], object@range['maxfbar']))
+    snames <- dimnames(object@catch)
+    dnames <- dimnames(object@catch.n)
+    dnames[['year']] <- '1'
+
+    # mean
+    if(mean == 'arithmetic')
+      foo <- function(x) ifelse(all(is.na(x)), 0, mean(x, na.rm=na.rm))
+    else if (mean =='geometric')
+      foo <- function(x) ifelse(all(is.na(x)), 0, exp(mean(log(x), na.rm=na.rm)))
+
+    # scaling
+    # 1. harvest values are divided for that year fbar (mean harvest for fages)
+    scaling  <- sweep(object@harvest[,fyears], 2:6, apply(object@harvest[fages,fyears] ,
+      2:6, 'mean', na.rm=na.rm), "/")
+    # 2. mean across fyears. All years are thus given equal weight
+    scaling <- apply(scaling, c(1,3:6), foo)
+
+    # NEW FLBRP
+    res <- new('FLBRP',
+      # range
+      range=object@range[c('min', 'max', 'plusgroup', 'minfbar', 'maxfbar')],
+
+      # fbar
+      fbar=FLQuant(fbar, units=units(object@harvest), quant=dims$quant),
+
+      # slots to be mean of byears
+      # (m, mat, harvest,spwn, m.spwn, discards.wt, landings.wt)
+      m = apply(object@m[,byears], c(1,3:6), foo),
+      mat = apply(object@mat[,byears], c(1,3:6), foo),
+      stock.wt = apply(object@stock.wt[,byears], c(1,3:6), foo),
+      harvest.spwn = apply(object@harvest.spwn[,byears], c(1,3:6), foo),
+      m.spwn = apply(object@m.spwn[,byears], c(1, 3:6), foo),
+      discards.wt = apply(object@discards.wt[,byears], c(1,3:6), foo),
+      landings.wt = apply(object@landings.wt[,byears], c(1,3:6), foo),
+
+      # rec.obs
+      rec.obs = object@stock.n[ac(dims$min)],
+
+      # ssb.obs
+      ssb.obs= ssb(object),
+
+      # landings & discards
+      landings.obs = object@landings,
+      discards.obs = object@discards,
+
+      # fbar.obs
+      fbar.obs = fbar(object),
+
+      # profit.obs
+      profit.obs = FLQuant(dimnames=snames),
+      
+      # discards.sel & landings.sel
+      discards.sel = scaling * apply(object@discards.n[,syears]/
+        (object@discards.n[,syears] + object@landings.n[,syears]), c(1,3:6), foo),
+      landings.sel = scaling * apply(object@landings.n[,syears]/
+        (object@discards.n[,syears] + object@landings.n[,syears]), c(1,3:6), foo),
+
+      # bycatch.wt & bycatch.harvest
+      bycatch.wt = FLQuant(0, dimnames=dnames),
+      bycatch.harvest = FLQuant(0, dimnames=dnames, units=units(object@harvest)),
+
+      # availability
+      availability = FLQuant(1, dimnames=dnames),
+
+      # price
+      price = FLQuant(as.numeric(NA), dimnames=dnames),
+
+      # model & params
+      model = model,
+      params = params
+    )
+
+    # extra args
+    args <- list(...)
+    for (i in names(args))
+      slot(res, i) <- args[[i]]
+
+    return(res)
+  }
+) # }}}
---
+++ branches/IM_FLBRP_20100121_Reformat_documentation/R/refpts.R
@@ -0,0 +1,203 @@
+# refpts - methods for the refpts class
+# FLBRP/R/refpts.R
+
+# Copyright 2003-2008 FLR Team. Distributed under the GPL 2 or later
+# Maintainers: Laurence Kell, Cefas & Santiago Cerviño, IEO
+# Last Change: 02 Apr 2009 11:08
+# $Id$
+
+# constructors  {{{
+if (!isGeneric("refpts"))
+	setGeneric("refpts", function(object, ...)
+		standardGeneric("refpts"))
+
+# refpts(array)
+setMethod('refpts', signature(object='array'),
+  function(object, refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'), iter=1, ...)
+  {
+    # reshape object for iter
+    if(length(dim(object)) < 3)
+      object <- array(object, dim=c(dim(object), iter))
+    else if (dim(object)[3] < iter)
+      object <- array(object, dim=c(dim(object)[-3], iter))
+    
+    # add dimnames
+    if(is.null(dimnames(object)))
+    {  
+      # default dnames
+      dmns <- list(
+        refpt=refpt,
+        quantity=c('harvest', 'yield', 'rec', 'ssb', 'biomass', 'revenue', 'cost',
+          'profit')[1:dim(object)[2]],
+        iter=1:dim(object)[3])
+        
+      dimnames(object) <- dmns
+      }
+
+
+     return(
+     new('refpts', object)
+     )
+  }
+)
+setMethod('refpts', signature(object='missing'),
+  function(refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'), iter=1, ...)
+  {
+    if(length(iter) == 1 && ac(iter) == '1')
+      iter <- seq(length=as.numeric(iter))
+    refpts(array(as.numeric(NA), dim=c(length(refpt), 8, length(iter))),
+      refpt=refpt, iter=iter, ...)
+  }
+)
+
+setMethod('refpts', signature(object='numeric'),
+  function(object, refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'), iter=1, ...)
+  {
+    refpts(array(as.numeric(object), dim=c(length(refpt), 8, iter)), refpt=refpt, 
+      iter=iter, ...)
+  }
+)
+
+setMethod('refpts', signature(object='logical'),
+  function(object, refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'), iter=1, ...)
+  {
+    refpts(array(as.numeric(object), dim=c(length(refpt), 8, iter)), refpt=refpt,
+      iter=iter, ...)
+  }
+)
+
+setMethod('refpts', signature(object='refpts'),
+  function(object, ...)
+    {
+    refpts(object@.Data, refpt=dimnames(object)$refpt,...)
+    })
+
+# }}}
+
+# show {{{
+setMethod('show', signature(object='refpts'),
+  function(object)
+  {
+		cat("An object of class \"refpts\":\n")
+
+       if(dim(object)[3] > 1){
+   		  v1 <- apply(object, 1:2, median, na.rm=TRUE)
+     		v2 <- apply(object, 1:2, mad,    na.rm=TRUE)
+        v3 <- paste(format(v1,digits=5),"(", format(v2, digits=3), ")", sep="")}
+      else
+        v3 <- paste(format(apply(object, 1:2, median, na.rm=TRUE),digits=5))
+
+    print(array(v3, dim=dim(object)[1:2], dimnames=dimnames(object)[1:2]), quote=FALSE)
+
+		if(dim(object)[3] != 1)
+			cat("iters: ", dim(object)[3],"\n\n")
+  
+  }
+) # }}}
+
+# propagate {{{
+setMethod('propagate', signature(object='refpts'),
+  function(object, iter, fill.iter=TRUE)
+    {
+    res <- refpts(object, iter=iter)
+    if(fill.iter== FALSE)
+      res[,,2:iter] <- as.numeric(NA)
+    return(res)
+   }
+) # }}}
+
+# refpts<-  {{{
+if (!isGeneric("refpts<-"))
+	setGeneric("refpts<-", function(object, ..., value)
+		standardGeneric("refpts<-"))
+
+setMethod('refpts<-', signature(object='FLBRP', value='refpts'),
+  function(object, value)
+  {
+    slot(object, 'refpts') <- value
+    return(object)
+  }
+)
+setMethod('refpts<-', signature(object='FLBRP', value='numeric'),
+  function(object, ..., value)
+  {
+    args <- list(...)
+
+    # selection required
+    if(length(args) > 0)
+    { 
+      # match and sort args names
+      if(!is.null(names(args)))
+        args <- args[match(names(dimnames(refpts(object))), names(args))]
+      names(args) <- c('i', 'j', 'k')[seq(length(args))]
+      args <- args[!unlist(lapply(args, is.null))]
+      args <- lapply(args, as.character)
+
+      refpts(object) <- do.call('[<-', c(list(x=refpts(object)), args, list(value=value)))
+    }
+    else
+    {
+      refpts(object)[] <- value
+    }
+    return(object)
+  }
+)
+
+# }}}
+
+# refpts  {{{
+setMethod('refpts', signature(object='FLBRP'),
+  function(object, ...)
+  {
+    args <- list(...)
+    refpts <- slot(object, 'refpts')
+    
+    # selection required
+    if(length(args) > 0)
+    {
+      # match and sort args names
+      if(!is.null(names(args)))
+        args <- args[match(names(dimnames(refpts)), names(args))]
+      names(args) <- c('i', 'j', 'k')[seq(length(args))]
+      args <- args[!unlist(lapply(args, is.null))]
+      args <- lapply(args, as.character)
+
+      return(do.call('[', c(list(x=refpts), args)))
+      
+    }
+    else
+      return(refpts)
+  }
+) # }}}
+
+# recalculations  {{{
+
+# MSY
+msy <- function(object)
+  {
+  refpts(object) <- refpts(as.numeric(NA), refpt='msy', iter=as.numeric(dimnames(object)$iter))
+  computeRefpts(object)
+  }
+
+# f0.1
+f0.1 <- function(object)
+  {
+  refpts(object) <- refpts(as.numeric(NA), refpt='f0.1', iter=as.numeric(dimnames(object)$iter))
+  computeRefpts(object)
+  }
+
+# fmax
+fmax <- function(object)
+  {
+  refpts(object) <- refpts(as.numeric(NA), refpt='fmax', iter=as.numeric(dimnames(object)$iter))
+  computeRefpts(object)
+  }
+
+# spr
+sprr <- function(object, spr='.30')
+  {
+  refpts(object) <- refpts(as.numeric(NA), refpt=paste('spr', sub('0.', '.', ac(spr)),sep=''), iter=as.numeric(dimnames(object)$iter))
+  computeRefpts(object)
+  }
+
+# }}}
