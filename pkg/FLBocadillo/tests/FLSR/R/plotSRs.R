fe88332a8a77f03853d3fe570666bb5d2a3d1c2a
lauriekell (Dr ) <laurieKell@gmail.com>
2010-01-28 13:15:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@425 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FLBocadillo/tests/FLSR/R/plotSRs.R
+++ pkg/FLBocadillo/tests/FLSR/R/plotSRs.R
@@ -10,7 +10,7 @@ plotSR<-function(sr,maxSSB,cols=rainbow(9)){
 
 plotSPR<-function(sr,ssb,maxSSB,cols=rainbow(9)){
   ssb<-FLQuant(seq(0,maxSSB,length.out=100))
-  plot(rec(sr[[1]])/ssb(sr[[1]])~ssb(sr[[1]]),xlim=c(0,maxSSB),xlab="SSB",ylab="Recruits",pch=19)
+  plot(rec(sr[[1]])/ssb(sr[[1]])~ssb(sr[[1]]),xlim=c(0,maxSSB),xlab="SSB",ylab="Recruits/SSB",pch=19)
   res<-lines(lowess(ssb(sr[[1]]),rec(sr[[1]])/ssb(sr[[1]])))
 
   for (i in 1:length(sr))
--- pkg/FLBocadillo/tests/FLSR/R/srModelOld.R
+++ pkg/FLBocadillo/tests/FLSR/R/srModelOld.R
@@ -23,15 +23,15 @@ par(mfrow=c(3,6),mar=c(4,4,1,1))
 #### Cod ###############################################################################################################
 cod4SR<-FLlst()
 
+cod4SR[["segreg"  ]]<-as.FLSR(cod4[["ices"]],model=segreg)
 cod4SR[["bevholt" ]]<-as.FLSR(cod4[["ices"]],model=bevholt)
 cod4SR[["ricker"  ]]<-as.FLSR(cod4[["ices"]],model=ricker)
-cod4SR[["segreg"  ]]<-as.FLSR(cod4[["ices"]],model=segreg)
 cod4SR[["shepherd"]]<-as.FLSR(cod4[["ices"]],model=shepherd)
 
 ## Fit
+cod4SR[["segreg"  ]]<-fmle(cod4SR[["segreg"  ]]) #, control=list(parscale=autoParscale(cod4SR[["segreg"  ]])))
 cod4SR[["bevholt" ]]<-fmle(cod4SR[["bevholt" ]], control=list(parscale=autoParscale(cod4SR[["bevholt" ]])))
 cod4SR[["ricker"  ]]<-fmle(cod4SR[["ricker"  ]]) #, control=list(parscale=autoParscale(cod4SR[["ricker"  ]])))
-cod4SR[["segreg"  ]]<-fmle(cod4SR[["segreg"  ]]) #, control=list(parscale=autoParscale(cod4SR[["segreg"  ]])))
 cod4SR[["shepherd"]]<-fmle(cod4SR[["shepherd"]], control=list(parscale=autoParscale(cod4SR[["shepherd"]])))
 
 ## Plotting
@@ -45,15 +45,15 @@ for (i in names(cod4SR)){
 #### Plaice  ###########################################################################################################
 ple4SR<-FLlst()
 
+ple4SR[["segreg"  ]]<-as.FLSR(ple4[["ices"]],model=segreg)
 ple4SR[["bevholt" ]]<-as.FLSR(ple4[["ices"]],model=bevholt)
 ple4SR[["ricker"  ]]<-as.FLSR(ple4[["ices"]],model=ricker)
-ple4SR[["segreg"  ]]<-as.FLSR(ple4[["ices"]],model=segreg)
 ple4SR[["shepherd"]]<-as.FLSR(ple4[["ices"]],model=shepherd)
 
 ## Fit
+ple4SR[["segreg"  ]]<-fmle(ple4SR[["segreg"  ]]) #, control=list(parscale=autoParscale(ple4SR[["segreg"  ]])))
 ple4SR[["bevholt" ]]<-fmle(ple4SR[["bevholt" ]], control=list(parscale=autoParscale(ple4SR[["bevholt" ]])))
 ple4SR[["ricker"  ]]<-fmle(ple4SR[["ricker"  ]]) #, control=list(parscale=autoParscale(ple4SR[["ricker"  ]])))
-ple4SR[["segreg"  ]]<-fmle(ple4SR[["segreg"  ]]) #, control=list(parscale=autoParscale(ple4SR[["segreg"  ]])))
 ple4SR[["shepherd"]]<-fmle(ple4SR[["shepherd"]], control=list(parscale=autoParscale(ple4SR[["shepherd"]])))
 
 ## Plotting
@@ -68,15 +68,15 @@ for (i in names(ple4SR)){
 #### Herring ###########################################################################################################
 her4SR<-FLlst()
 
+her4SR[["segreg"  ]]<-as.FLSR(her4[["ices"]],model=segreg)
 her4SR[["bevholt" ]]<-as.FLSR(her4[["ices"]],model=bevholt)
 her4SR[["ricker"  ]]<-as.FLSR(her4[["ices"]],model=ricker)
-her4SR[["segreg"  ]]<-as.FLSR(her4[["ices"]],model=segreg)
 her4SR[["shepherd"]]<-as.FLSR(her4[["ices"]],model=shepherd)
 
 ## Fit
+her4SR[["segreg"  ]]<-fmle(her4SR[["segreg"  ]]) #, control=list(parscale=autoParscale(her4SR[["segreg"  ]])))
 her4SR[["bevholt" ]]<-fmle(her4SR[["bevholt" ]], control=list(parscale=autoParscale(her4SR[["bevholt" ]])))
 her4SR[["ricker"  ]]<-fmle(her4SR[["ricker"  ]], control=list(parscale=autoParscale(her4SR[["ricker"  ]])))
-her4SR[["segreg"  ]]<-fmle(her4SR[["segreg"  ]]) #, control=list(parscale=autoParscale(her4SR[["segreg"  ]])))
 her4SR[["shepherd"]]<-fmle(her4SR[["shepherd"]], control=list(parscale=autoParscale(her4SR[["shepherd"]])))
 
 ## Plotting
