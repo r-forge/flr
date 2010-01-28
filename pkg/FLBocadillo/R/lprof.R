01b7e086674f9a744ff334b8fa92477692a27bdd
lauriekell (Dr ) <laurieKell@gmail.com>
2010-01-28 11:44:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@422 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FLBocadillo/R/lprof.R
+++ pkg/FLBocadillo/R/lprof.R
@@ -4,7 +4,7 @@ setGeneric("lprof", function(object, ...){
 
 setMethod('lprof',
   signature(object='FLSR'),
-    function(object, .plot=TRUE, .soultion=TRUE, .scaling=c("rel","rel"),.parscale=NULL,...){
+    function(object, .plot=TRUE, .solution=TRUE, .scaling=c("rel","rel"),.parscale=NULL,.CI=c(.5,.75,.9,.95),xlab=NULL,ylab=NULL,...){
 
 if (FALSE) {
             object<-cod4SR[["bevholtSV"]]
@@ -15,6 +15,16 @@ if (FALSE) {
             s =seq(0.5,1.5,length.out=10)
             v =seq(0.5,1.5,length.out=10)
             args<-list(s=s,v=v,fixed=list(spr0=0.45))}
+
+if (FALSE) {
+            fixed<-list(spr0=spr0(cod4[[1]]))
+            object<-cod4SR[["shepherdSV"]]
+            plot=TRUE
+            .solution=TRUE
+            .scaling=c("abs")
+            .parscale="c"
+            c=seq(1.0,5.0,length.out=10)
+            args<-list(c=c,fixed=fixed)}
 
 
       ## function to get parameter coordinate system
@@ -35,24 +45,37 @@ if (FALSE) {
     ## 1D profile
     if (length(pars) == 1){
       ## create a grid centred on best guess
-      rng1<-args[[pars[1]]]*params(object)[pars[1],1]
+      .scaling<-getScale(.scaling,pars)
+
+      ## create grid
+      rng1<-args[[pars[1]]]
+
+      ## specify rnages as relative proportions
+      if (substr(.scaling[1],1,1)=="r") rng1<-rng1*params(object)[pars[1],1]
 
       paramGrid<-expand.grid(rng1,ll=NA)
       names(paramGrid)[1]<-pars
-
+      
+      #if ("start" %in% names(formals(lprof))) .start<-start else
+                                              .start<-as.list(params(object)[,drop=T])
+                                              
       ## profile and plot liklihood around best guess
       for (i in 1:length(paramGrid[,1])){
         fixed=(list(paramGrid[i,pars[1]]))
         names(fixed)<-pars
 
+        if ("fixed" %in% names(args)) fixed=c(fixed,args[names(args)=="fixed"][[1]])
+
         psPar<-dimnames(params(object))$params[dimnames(params(object))$params!=pars]
-        paramGrid[i,"ll"]<-logLik(fmle(object,fixed=fixed,control=list(.parscale=auto.parscale(object)[psPar])))}
+        psPar<-dimnames(params(object))$params[dimnames(params(object))$params!=pars]
+        paramGrid[i,"ll"]<-logLik(fmle(object,start=.start,fixed=fixed,control=list(parscale=autoParscale(object)[psPar])))}
 
       ## plot liklihood
-      if (plot){
-        plot(  paramGrid[,"ll"]~paramGrid[,1],xlab=pars[1],ylab="Likelihood",type="l")
+      if (.plot){
+        if (is.null(xlab)) xlab=pars[1]
+        plot(  paramGrid[,"ll"]~paramGrid[,1],xlab=xlab,ylab="Likelihood",type="l")
 
-        if (.soultion)
+        if (.solution)
            points(logLik(object)~params(object)[pars[1],1],pch=19)}
 
     ## 2D profile
@@ -60,6 +83,8 @@ if (FALSE) {
 
       .scaling<-getScale(.scaling,pars)
 
+      #if ("start" %in% names(formals(lprof))) .start<-start else
+                                              .start<-as.list(params(object)[,drop=T])
       ## create grid
       rng1<-args[[pars[1]]]
       rng2<-args[[pars[2]]]
@@ -101,16 +126,25 @@ if (FALSE) {
            paramGrid[i,"ll"]<-do.call("logl.",arg)
         }else{
            psPar<-dimnames(params(object))$params[!(dimnames(params(object))$params %in% pars)]
-           paramGrid[i,"ll"]<-logLik(fmle(object,fixed=fixed,control=list(.parscale=as.list(autoParscale(object)[psPar]))))}
-      }
-      
-      ## plot liklihood"
-      if (plot){
-        plotGrid<-list(x=sort(unique(paramGrid[,1])), y=sort(unique(paramGrid[,2])), z=tapply(paramGrid[,"ll"], list(paramGrid[,1],paramGrid[,2]),mean))
-        image(  plotGrid,xlab=pars[1],ylab=pars[2])
-        contour(plotGrid,add=T)
+           psPar<-psPar[!(psPar %in% names(fixed))]
 
-        if (.soultion)
+           paramGrid[i,"ll"]<-logLik(fmle(object,fixed=fixed,start=.start,control=list(  parscale=as.list(autoParscale(object)[psPar]))))}
+      }
+
+      ## plot liklihood"
+      if (.plot){
+         plotGrid<-list(x=sort(unique(paramGrid[,1])), y=sort(unique(paramGrid[,2])), z=tapply(paramGrid[,"ll"], list(paramGrid[,1],paramGrid[,2]),mean))
+         CI<-logLik(object)-qchisq(.CI,2)
+         if (is.null(xlab)) xlab=pars[1]
+         if (is.null(ylab)) ylab=pars[2]
+
+         image(  plotGrid,ylab=ylab, xlab=xlab) #, breaks=CI,col=rainbow(length(CI)-1))
+         contour(plotGrid,  levels=CI,add=T, col="grey",  lwd=2, labels=CI)
+   
+        #image(  plotGrid,xlab=pars[1],ylab=pars[2])
+        #contour(plotGrid,add=T)
+
+        if (.solution)
           points(params(object)[pars[1],1,drop=T],params(object)[pars[2],1,drop=T],pch=19)}
 
     }else if (length(pars) > 2 | length(pars) == 0) stop("need to specify 1 or 2 valid parameters")
