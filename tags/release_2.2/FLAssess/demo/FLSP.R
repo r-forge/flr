6c4af425986b8101ce20522750e3b459f558ad24
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2010-02-22 15:29:36 +0000

    Release 2.2 for FLAssess
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@474 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ tags/release_2.2/FLAssess/demo/FLSP.R
@@ -0,0 +1,20 @@
+# FLSP - «Short one line description»
+# FLSP
+
+# Copyright 2003-2008 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira, Cefas
+# Last Change: 19 Jun 2008 18:31
+# $Id$
+
+# Reference:
+# Notes:
+
+# TODO Thu 19 Jun 2008 06:24:56 PM CEST IM:
+
+library(FLAssess)
+
+data(nhke)
+
+summary(nhke)
+
+
