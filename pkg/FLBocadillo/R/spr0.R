782c6f10611a8b1e0dd65466d549771ada9c127c
lauriekell (Dr ) <laurieKell@gmail.com>
2010-04-06 16:26:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@646 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/FLBocadillo/R/spr0.R
+++ pkg/FLBocadillo/R/spr0.R
@@ -1,8 +1,11 @@
 ## calcs spawner per recruit at F=0.0
+if (!isGeneric("spr0"))
+  setGeneric("spr0", function(ssb, rec, fbar, ...)
+	  standardGeneric("spr0"))
 
 setMethod('spr0', signature(ssb='FLQuant', rec='FLQuant', fbar='FLQuant'),
-   function(ssb, rec, fbar){
-
+   function(ssb, rec, fbar,log=TRUE){
+   
     if  (any(dim(ssb)[3:5]>1)) "stop multiple units, seasons, areas not allowed yet"
     if  (any(dim(rec)[3:5]>1)) "stop multiple units, seasons, areas not allowed yet"
     if  (any(dim(fbar)[3:5]>1)) "stop multiple units, seasons, areas not allowed yet"
@@ -19,10 +22,20 @@ setMethod('spr0', signature(ssb='FLQuant', rec='FLQuant', fbar='FLQuant'),
     fbar <- fbar[1, as.character(seq(minyear, maxyear)), drop=TRUE]
 
     # spr0
-    spr0 <- lm(c(ssb/rec)~c(fbar))$coefficients[1]
-    names(spr0) <- "spr0"
+    dmns     <-dimnames(ssb(x))
+    dmns$year<-1
+    res      <-FLQuant(NA,dimnames=dmns)
 
-    return(spr0)})
+    if (log) {
+       calcSpr0 <-function(x) exp(lm(log(c(ssb(x)/rec(x)))~c(fbar(x)))$coefficients[1])
+       for (i in dmns$iter)
+         res[,,,,,i]<-calcSpr0(iter(x,i))}
+    else {
+       calcSpr0 <-function(x) lm(c(ssb(x)/rec(x))~c(fbar(x)))$coefficients[1]
+       for (i in dmns$year)
+         res[,,,,,i]<-calcSpr0(iter(x,i))}
+
+    return(res)})
 
 setMethod('spr0', signature(ssb='FLStock', rec='missing', fbar='missing'),
   function(ssb){
