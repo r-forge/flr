a8f49df302e9d81cf89f846741484d67a2022213
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2010-05-31 15:09:48 +0000

    Deleted old branches
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@683 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/IM_FLBRP_20100319_Plots/R/kobe.R
+++ branches/IM_FLBRP_20100319_Plots/R/kobe.R
@@ -13,13 +13,22 @@ setGeneric('kobe', function(biomass, harvest, refpts, ...)
 setMethod("kobe", signature(biomass="FLBRP", harvest="missing", refpts="missing"),
     function(biomass, ...)
     {
-      kobe(biomass=ssb.obs(biomass), harvest=fbar.obs(biomass), refpts=biomass@refpts,
-        ...)
+      kobe(biomass=ssb.obs(biomass), harvest=fbar.obs(biomass),
+          refpts=biomass@refpts['msy',], ...)
     }
 ) # }}}
 
 # kobe(FLBRP) {{{
 setMethod("kobe", signature(biomass="FLBRP", harvest="missing", refpts="list"),
+    function(biomass, refpts, ...)
+    {
+      kobe(biomass=ssb.obs(biomass), harvest=fbar.obs(biomass), refpts=refpts,
+        ...)
+    }
+) # }}}
+
+# kobe(FLBRP) {{{
+setMethod("kobe", signature(biomass="FLBRP", harvest="missing", refpts="refpts"),
     function(biomass, refpts, ...)
     {
       kobe(biomass=ssb.obs(biomass), harvest=fbar.obs(biomass), refpts=refpts,
@@ -43,8 +52,9 @@ setMethod("kobe", signature(biomass="FLQuant", harvest="FLQuant", refpts="list")
 
 # kobe(FLQuant, FLQuant, refpts) {{{
 setMethod("kobe", signature(biomass="FLQuant", harvest="FLQuant", refpts="refpts"),
-  function(biomass, harvest, refpts, xlab=expression(SSB:SSB[MSY]),
-      ylab=expression(F:F[MSY]), ...)
+  function(biomass, harvest, refpts, type='b', lines=list(col='black'),
+      points=list(pch=21, bg=gray(seq(0.8, 0.2, length=dim(biomass)[2])), cex=1.5), 
+      xlab=expression(SSB:SSB[MSY]), ylab=expression(F:F[MSY]), add=FALSE,  ...)
   {
     # check refpts
     if(dim(refpts)[1] > 1)
@@ -81,32 +91,91 @@ setMethod("kobe", signature(biomass="FLQuant", harvest="FLQuant", refpts="refpts
     firstyear <- dimnames(biomass)$year[1]
     lastyear <- dimnames(biomass)$year[dim(biomass)[2]]
 
-    # initial plot
-    plot(1, 1, type='b', xlim=xlim, ylim=ylim, xlab=xlab, ylab=ylab)
+    if(!add)
+    {
+      # initial plot
+      plot(1, 1, type=type, xlim=xlim, ylim=ylim, xlab=xlab, ylab=ylab, ...)
 
-    # actual limits
-    parlim<- par()$usr
+      # actual limits
+      parlim<- par()$usr
 
-    # polygons
-    # green
-    polygon(c(1, 1, parlim[2], parlim[2]), c(parlim[3], 1, 1, parlim[3]), col='green')
-    # yellow
-    polygon(c(1, parlim[1], parlim[1], 1), c(parlim[3], parlim[3], 1, 1), col='yellow')
-    # red
-    polygon(c(parlim[1], parlim[1], 1, 1), c(1, parlim[4], parlim[4], 1), col='red')
-    # orange
-    polygon(c(1, 1, parlim[2], parlim[2]), c(1, parlim[4], parlim[4], 1), col='orange')
+      # polygons
+      # green
+      polygon(c(1, 1, parlim[2], parlim[2]), c(parlim[3], 1, 1, parlim[3]), col='green')
+      # yellow
+      polygon(c(1, parlim[1], parlim[1], 1), c(parlim[3], parlim[3], 1, 1), col='yellow')
+      # red
+      polygon(c(parlim[1], parlim[1], 1, 1), c(1, parlim[4], parlim[4], 1), col='red')
+      # orange
+      polygon(c(1, 1, parlim[2], parlim[2]), c(1, parlim[4], parlim[4], 1), col='orange')
+    
+      # year labels
+      text(mbiomass[,lastyear]+mbiomass[,lastyear]*0.1, mharvest[,lastyear], lastyear)
+      text(mbiomass[,firstyear]+mbiomass[,firstyear]*0.1, mharvest[,firstyear], firstyear)
+    }
 
     # points
-    lines(mbiomass, mharvest)
-    points(biomass[,lastyear], harvest[,lastyear], cex=1, pch=21, bg='blue')
-    points(mbiomass, mharvest, pch=21, bg=gray(seq(0.9, 0.2, length=length(biomass))),
-      cex=1.5)
-    text(mbiomass[,lastyear]+mbiomass[,lastyear]*0.1, mharvest[,lastyear], lastyear)
-    text(mbiomass[,firstyear]+mbiomass[,firstyear]*0.1, mharvest[,firstyear], firstyear)
+    if(type %in% c('l', 'b'))
+      do.call('lines', c(list(x=mbiomass, y=mharvest), lines))
+
+    if(type %in% c('p', 'b'))
+    {
+      points(biomass[,lastyear], harvest[,lastyear], cex=1, pch=21, bg='blue')
+      do.call('points', c(list(x=mbiomass, y=mharvest), points))
+    }
   }
 ) # }}}
 
-# kobeProb
+setGeneric('kobeProb', function(biomass, harvest, refpts, ...)
+	standardGeneric('kobeProb'))
+
+# kobeProb  {{{
+setMethod('kobeProb', signature(biomass='FLQuant', harvest='FLQuant', refpts='refpts'),
+  function(biomass, harvest, refpts, xlab='probability', ylab='', lwd=1.5, ...)
+  {
+    # check refpts
+    if(dim(refpts)[1] > 1)
+      stop("kobe can only plot relative to a single reference point")
+
+    # check dims
+    dims <- c(biomass=dim(biomass)[6], harvest=dim(harvest)[6], refpts=dim(refpts)[3])
+
+    # refpts not 1?
+    if(dims['refpts'] > 1)
+      stop('kobeProb can only deal with a single reference point')
+
+    if(dims[1] == 1 & dims[2] == 1)
+      warning('no iters in biomass or harvest, plot might not make sense')
+
+    # biomass & harvest not equal?
+    dimt <- dims['biomass'] / dims['harvest']
+    if(dimt != 1 & dimt != max(dims[1:2]))
+      stop('biomass and harvest iters do not match')
+
+    iters <- max(dims)
+    
+    # indices
+    biomass <- sweep(biomass, 6, refpts[, 'ssb'], '/', check.margin=FALSE)
+    harvest <- sweep(harvest, 6, refpts[, 'harvest'], '/', check.margin=FALSE)
+    years <- dimnames(biomass)$year
+
+    # red
+    red <- apply(biomass < 1 & harvest < 1, 1:5, sum) / iters
+
+    # green
+    green <- apply(biomass > 1 & harvest > 1, 1:5, sum) / iters
+
+    # yellow
+    yellow <- apply((biomass > 1 & harvest < 1) | (biomass < 1 & harvest > 1),
+      1:5, sum) / iters
+
+    # plot
+    plot(years, red, type='l', col='red', lwd=lwd, ylim=c(0,1), xlab=xlab, ylab=ylab)
+    lines(years, yellow, col='yellow', lwd=lwd)
+    lines(years, green, col='green', lwd=lwd)
+  }
+) # }}}
+
+
 
 # kobeMatrix
