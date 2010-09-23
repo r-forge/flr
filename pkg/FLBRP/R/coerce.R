17b4e8d9bd7eb70c15740c8d5ea79672947826d4
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2010-09-23 15:14:26 +0000

    Moved as.FLSR to coerce.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@752 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 2d78e36c9aafe2cdf201b582c3d96608b800ac7c

--- pkg/FLBRP/R/coerce.R
+++ pkg/FLBRP/R/coerce.R
@@ -3,7 +3,7 @@
 
 # Copyright 2003-2009 FLR Team. Distributed under the GPL 2 or later
 # Maintainers: Laurence Kell, Cefas & Santiago Cerviño, IEO
-# Last Change: 31 Mar 2010 13:32
+# Last Change: 23 Sep 2010 16:38
 # $Id$
 
 # as.FLSR {{{
@@ -79,3 +79,48 @@ setAs('FLBRP', 'FLStock',
       stop("invalid object created. Please check input object")
   }
 ) # }}}
+
+setMethod("as.data.frame", 
+signature(x="FLBRP", row.names="ANY", optional="character"),
+  function(x, row.names=NULL, optional)
+  {
+    if(any(c('model', 'params', 'refpts', 'name', 'desc', 'range') %in% optional))
+      stop("Only 'FLQuant' slots can be converted")
+    df <- function(x, slots, names=slots)
+    {
+      res <-FLQuants(mlply(slots, function(x,fl)
+        do.call(x,list(fl)), fl=x))
+
+      names(res)<-slots
+      return(
+          as.data.frame(res, row.names=row.names)
+          )
+    }
+
+    return(df(x,optional))
+  }
+)
+
+setMethod("as.FLSR", signature(object="FLBRP"),
+  function(object, ...){
+
+	  rec.age  <-range(object,"min")
+	  recYrCls <-as.numeric(dimnames(rec.obs(object))$year)-rec.age
+    ssbYrCls <-as.numeric(dimnames(ssb.obs(object))$year)
+
+    ssbYrCls<-ssbYrCls[ssbYrCls %in% recYrCls]
+    recYrCls<-ssbYrCls+rec.age
+
+    # calculate ssb and create FLSR object incorprating rec.age
+    rec <- rec.obs(object)[,ac(recYrCls)]
+    ssb <- ssb.obs(object)[,ac(ssbYrCls)]
+
+   # create the FLSR object
+   sr = FLSR(name    =object@name,
+	           rec     =rec,
+             ssb     =ssb,
+             desc    ="'rec' and 'ssb' slots obtained from a 'FLBRP' object", ...)
+
+    validObject(sr)
+    return(sr)
+    })
