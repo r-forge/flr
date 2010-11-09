14569f7c516b5443b65325e73d72d26588d2aa1d
lauriekell (Dr ) <laurieKell@gmail.com>
2010-11-09 18:09:59 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@789 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    Former-commit-id: f9dbc44f3eb7404c85c4f1e93ad71fdfb208af01

--- pkg/FLR4SCRS/R/srr.R
+++ pkg/FLR4SCRS/R/srr.R
@@ -2,29 +2,29 @@
 #### "Simpler" SRR methods #####################################################
 ################################################################################
 
-setGeneric('srr', function(model,params,ssb,rec, ...)
+setGeneric('srr', function(params,ssb,model, ...)
   standardGeneric('srr'))
   
-setMethod("srr", signature(model="character",params="FLPar",ssb="numeric",rec="numeric"),
-   function(model,params,ssb,rec) "do.call stuff" )
+setMethod("srr", signature(params="FLPar",ssb="numeric",model="character",),
+   function(params,ssb,model) "do.call stuff" )
 
-setMethod("srr", signature(model="formula",  params="FLPar",ssb="numeric",rec="numeric"),
-   function(model,params,ssb,rec) "do.call stuff" )
+setMethod("srr", signature(params="FLPar",ssb="numeric",model="formula"),
+   function(params,ssb,model) "do.call stuff" )
 
-setMethod("srr", signature(model="character",params="FLPar",ssb="FLQuant",rec="FLQuant"),
-   function(model,params,ssb,rec) "do.call stuff" )
+setMethod("srr", signature(params="FLPar",ssb="FLQuant",model="character"),
+   function(params,ssb,model) "do.call stuff" )
 
-setMethod("srr", signature(model="formula",  params="FLPar",ssb="FLQuant",rec="FLQuant"),
-   function(model,params,ssb,rec) "do.call stuff" )
+setMethod("srr", signature(params="FLPar",ssb="FLQuant",model="formula"),
+   function(params,ssb,model) "do.call stuff" )
 
-setMethod("srr", signature(model="character",params="FLPar",ssb="FLStock"),
-   function(model,params,ssb,rec) "do.call stuff" )
+setMethod("srr", signature(params="FLPar",ssb="FLStock",model="character",),
+   function(params,ssb,model) "do.call stuff" )
 
-setMethod("srr", signature(model="formula",  params="FLPar",ssb="FLStock"),
-   function(model,params,ssb,rec) "do.call stuff" )
+setMethod("srr", signature(params="FLPar",ssb="FLStock",model="formula"),
+   function(params,ssb,model) "do.call stuff" )
 
-setMethod("srr", signature(model="character",params="FLPar",ssb="FLBiol"),
-   function(model,params,ssb,rec) "do.call stuff" )
+setMethod("srr", signature(params="FLPar",ssb="FLBiol",model="character"),
+   function(params,ssb,model) "do.call stuff" )
 
-setMethod("srr", signature(model="formula",  params="FLPar",ssb="FLBiol"),
-   function(model,params,ssb,rec) "do.call stuff" )
+setMethod("srr", signature(params="FLPar",ssb="FLBiol",model="formula"),
+   function(params,ssb,model) "do.call stuff" )
