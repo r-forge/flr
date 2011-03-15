017ae61ca9cbc8226d2f4fdea34eaa084ece9017
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2011-03-15 12:36:08 +0000

    Extensions with themes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@911 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 2884d401a715a3914ec16ad8fcf0624f38881934

--- pkg/FLR4SCRS/R/createFL.R
+++ pkg/FLR4SCRS/R/createFL.R
@@ -121,12 +121,6 @@ getFLQ<-function(filename,pos1, pos2){
 
     return(flq)}
 
-vpa2BoxFiles<-function(file){
-   i <- skip.hash(0,file)
-   j <- skip.until.hash(i,file)
-
-   gsub(" ","",gsub("'","",substr(scan(file,skip=i+1,nlines=j-i-1,quiet=TRUE,what=character(),sep="\n"),1,20)))}
-
 setGeneric("createFLStock",   function(x,type,...)
 	standardGeneric("createFLStock"))
 setGeneric("createFLStocks",  function(x,type,...)
@@ -152,7 +146,7 @@ setMethod("createFLStock",   signature(x="character",type="character"),
     return(res)})
 
 setMethod("createFLStocks",  signature(x="character",type="character"),
-function(x,type,n,...){
+  function(x,type,n,...){
     type<-toupper(type)
     legalFileType(type,valid=toupper(c("adapt","vpa2box")))
 
@@ -322,73 +316,109 @@ readBinary<-function(x,dmns=list(),size=4){
 
          return(res)}
 
+vpa2BoxFiles<-function(file){
+   i <- skip.hash(0,file)
+   j <- skip.until.hash(i,file)
+
+   res<-gsub(" ","",gsub("'","",substr(scan(file,skip=i+1,nlines=j-i-1,quiet=TRUE,what=character(),sep="\n"),1,20)))
+   
+   print(res)
+   return(res)}
+
+
 ## reads in data from VPA2Box files
-readVPA2Box<-function(x,nits=501,...){
-    files<-vpa2BoxFiles(x)
+readVPA2Box<-function(x,nits=501,...){ 
+
+  ## control file ##############################################################
+  dir  <-getDir(x)
+  files<-vpa2BoxFiles(x)
+  ##############################################################################
+print(files)
+  ## "csv" file ################################################################ 
+  # data 
+  dat<-scan(paste(dir,.Platform$file.sep,files[5],sep=""),what="",sep="\n",strip.white=TRUE )
+  dat<-dat[nchar(dat)>0]
+
+  # gets line number for start of data  
+  ln <-c(F=grep("F",dat)[1],
+         N=grep("N",dat)[1],
+         C=grep("C",dat)[1],
+         W=grep("W",dat)[1],
+         I=grep("I",dat)[2])
+  
+  # function to converts data in "csv" file into an FLQuant
+  aaIn<-function(aa,minage=1){
+    aa<-aa[nchar(aa)>1]
+    N <-length(aa)
+    aa<-unlist(strsplit(aa," +"))
+    aa<-aa[nchar(aa)>0]
+
+    dms<-c(length(aa)/N,N)
+
+    aa<-array(as.numeric(aa),dim=dms)
     
-    dir<-getDir(x)
-    dat<-scan(paste(dir,.Platform$file.sep,files[5],sep=""),what="",sep="\n")
-    dat<-dat[nchar(dat)>0]
+    FLQuant(c(aa[-1,]),dimnames=list(age=minage+(0:(dms[1]-2)),year=aa[1,]))}
 
-    ln <-c(F=grep("F",dat)[1],
-           N=grep("N",dat)[1],
-           C=grep("C",dat)[1],
-           W=grep("W",dat)[1],
-           I=grep("I",dat)[2])
 
-    aaIn<-function(aa,minage=1){
-           aa<-aa[nchar(aa)>1]
-           N <-length(aa)
-           aa<-unlist(strsplit(aa," +"))
-           aa<-aa[nchar(aa)>0]
+  stk<-FLStock(stock.n=aaIn(dat[(ln[2]+1):(ln[3]-1)]))
 
-           dms<-c(length(aa)/N,N)
+  harvest.     =aaIn(dat[(ln[1]+1):(ln[2]-1)])
+  landings.n.  =aaIn(dat[(ln[3]+1):(ln[4]-1)])
+  stock.wt.    =aaIn(dat[(ln[4]+1):(ln[5]-1)])
+  harvest(stk) =harvest.
 
-           aa<-array(as.numeric(aa),dim=dms)
+  landings.n( stk)   =landings.n.
+  stock.wt(   stk)   =stock.wt.
+  landings.wt(stk)   =stock.wt.
+  discards.wt(stk)[]=0
+  ##############################################################################
+  
+  ## data file #################################################################
+  ## Year range
+  i    <-0
+  i    <-skip.hash(i,files[1])
+  yrRng<-read.table(files[1],skip=i,nrows=1,sep="\n",colClasses="character",strip.white=TRUE)[[1,1]]
+  yrRng<-as.integer(strsplit(yrRng," +")[[1]][1:2])
 
-           FLQuant(c(aa[-1,]),dimnames=list(age=minage+(0:(dms[1]-2)),year=aa[1,]))}
+  ## age range
+  i     <-skip.hash(i,files[1])
+  ageRng<-read.table(files[1],skip=i,nrows=1,sep="\n",colClasses="character",strip.white=TRUE)[[1,1]]
+  ageRng<-as.integer(strsplit(ageRng," +")[[1]][1:4])
 
-      getFLQuant<-function(x,dimnames=NULL){
-          ## assume is a file name
-          if (is.character(x))  return()  else
-          if (is(x, 'FLQuant')) return(x) else
-          if(is.vector(x))
-            if(length(x) == 1)
-              return(FLQuant(x, dimnames=dimnames))
-            else{
-              if (length(dimnames[[1]])!=length(x)) warning("vector being recycled in FLQuant creator")
-              return(FLQuant(x, dimnames=dimnames))}
-          else if(is.data.frame(x))
-              return(as.FLQuant(x))
-          else if(is.matrix(x))
-              stop("not implemented yet")
-          else if(is.array(x))
-              stop("not implemented yet")}
+  ## number of indices
+  i<-skip.hash(i,files[1])
+  read.table(files[1],skip=i,nrows=1,sep="\n")
 
-   stk<-FLStock(stock.n=aaIn(dat[(ln[2]+1):(ln[3]-1)]))
+  ## xxx.spwn
+  i<-skip.hash(i,files[1])
+  x.spwn<-read.table(files[1],skip=i,nrows=1,sep="\n",colClasses="character",strip.white=TRUE)[[1,1]]
+  x.spwn<-as.integer(strsplit(x.spwn," +")[[1]][1])
+  x.spwn<-(x.spwn-0.5)/12
+  m.spwn(stk)[]     =x.spwn
+  harvest.spwn(stk) =m.spwn(stk)
 
-   harvest.   =aaIn(dat[(ln[1]+1):(ln[2]-1)])
-   landings.n.=aaIn(dat[(ln[3]+1):(ln[4]-1)])
-   stock.wt.  =aaIn(dat[(ln[4]+1):(ln[5]-1)])
-   harvest(   stk) =harvest.
+  ## mat
+  i     <-skip.hash(i,files[1])
+  mat   <-read.table(files[1],skip=i,nrows=1,sep="\n",colClasses="character",strip.white=TRUE)[[1,1]]
+  mat   <-as.integer(strsplit(mat," +")[[1]])
+  
+  mat(   stk)[]     = mat[1:dim(mat(stk))[1]]
+  ##############################################################################
 
-   landings.n(stk) =landings.n.
-   stock.wt(  stk) =stock.wt.
-   discards.wt(stk)[]<-0
+  ## Binary files ##############################################################
+  dmns     <-dimnames(stock.n(stk))
+  dmns$iter<-1:nits
 
-   dmns     <-dimnames(stock.n(stk))
-   dmns$iter<-1:nits
-
-   if (file.exists(paste(dir,"MAA.OUT",sep="/")))
-      m(stk)      <-readBinary(paste(dir,"MAA.OUT",sep="/"), dmns)
-   if (file.exists(paste(dir,"FAA.OUT",sep="/")))
-      harvest(stk)<-readBinary(paste(dir,"FAA.OUT",sep="/"), dmns)
-   if (file.exists(paste(dir,"NAA.OUT",sep="/")))
-     stk@stock.n  <-readBinary(paste(dir,"NAA.OUT",sep="/"), dmns)
-   if (file.exists(paste(dir,"CAA.OUT",sep="/")))
-     catch.n(stk) <-readBinary(paste(dir,"CAA.OUT",sep="/"), dmns)
-   else
-     catch.n(stk)     <-stock.n(stk)*harvest(stk)/(harvest(stk)+m(stk))*(1-exp(-((harvest(stk)+m(stk)))))
+  if (file.exists(paste(dir,"MAA.OUT",sep="/")))
+    m(stk)      <-readBinary(paste(dir,"MAA.OUT",sep="/"), dmns)
+  if (file.exists(paste(dir,"FAA.OUT",sep="/")))
+    harvest(stk)<-readBinary(paste(dir,"FAA.OUT",sep="/"), dmns)
+  if (file.exists(paste(dir,"NAA.OUT",sep="/")))
+    stk@stock.n  <-readBinary(paste(dir,"NAA.OUT",sep="/"), dmns)
+  if (file.exists(paste(dir,"CAA.OUT",sep="/")))
+    catch.n(stk) <-readBinary(paste(dir,"CAA.OUT",sep="/"), dmns)
+  else
+    catch.n(stk)     <-stock.n(stk)*harvest(stk)/(harvest(stk)+m(stk))*(1-exp(-((harvest(stk)+m(stk)))))
 
    units(harvest(stk))<-"f"
 
@@ -396,12 +426,10 @@ readVPA2Box<-function(x,nits=501,...){
    discards.n(stk)[]<-0
 
    if (file.exists(paste(x,"WAA.OUT",sep="/"))){
-     stock.wt(   stk)<-readBinary(paste(x,"WAA.OUT",sep="/"),dimnames(stock.n(stk)))
-     catch.wt(   stk)<-stock.wt(stk)
-     landings.wt(stk)<-stock.wt(stk)
-     discards.wt(stk)<-stock.wt(stk)}
-
-   wt(stk)          <-stock.wt(stk)
+    stock.wt(   stk)<-readBinary(paste(x,"WAA.OUT",sep="/"),dimnames(stock.n(stk)))
+    catch.wt(   stk)<-stock.wt(stk)
+    landings.wt(stk)<-stock.wt(stk)
+    discards.wt(stk)<-stock.wt(stk)}
 
    ## replace any slots
    args <- list(...)
@@ -409,7 +437,7 @@ readVPA2Box<-function(x,nits=501,...){
    for(i in names(args)[names(args) %in% slt]){
       slot(stk, i)<-args[[i]]}
 
-   catch(stk)<-computeCatch(      stk,"all")
+   catch(stk)   <-computeCatch(      stk,"all")
    landings(stk)<-computeLandings(stk)
    discards(stk)<-computeDiscards(stk)
 
@@ -419,13 +447,13 @@ readVPA2Box<-function(x,nits=501,...){
 
    return(stk)}
 
-## create retro stocks
+# create retro stocks
 getRetros<-function(dir,stk,fileNm,n){
     stks<-FLStocks()
 
     for (iRetro in 0:n){
        ## Start reading file
-       filename<-paste(dir,.Platform$file.sep,fileNm,iRetro,".R",sep="")
+       filename<-paste(dir,.Platform$file.sep,fileNm,iRetro,".r",sep="")
 
        ## get Retro estimates
        i<-0
--- pkg/FLR4SCRS/R/getClassMethods.R
+++
@@ -1,3 +0,0 @@
-getClassMethods<- function(class, where){
-     v<- showMethods(class=class, where=where, printTo=FALSE)->v
-     unlist(lapply(strsplit(unlist(lapply(strsplit(v[substr(v, 1, 5) %in% "Funct"], " \\("), "[", 1)), ": "), "[", 2))}
