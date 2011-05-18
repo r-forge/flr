b98087f4feb40c5cdd6a13f30223fec3d933b802
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2011-05-18 12:53:29 +0000

    Branch for IO funcs in FLCore
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@958 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: d9f4196661c052f784623409bcb854d95120f65c

---
+++ branches/IM_FLCore_20110518_io/tests/FLFleet.R
@@ -0,0 +1,11 @@
+# FLFleet - «Short one line description»
+# FLFleet
+# FLCore/tests/FLFleet
+
+# Copyright 2003-2008 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Iago Mosqueira, Cefas
+# $Id: FLFleet.R 14 2009-01-26 15:01:42Z imosqueira $
+
+library(FLCore)
+
+# BUG IM_20081212_1305 validity(FLFleet)
