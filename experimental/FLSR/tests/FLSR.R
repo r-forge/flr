16a9a400f849606c75a15570b7ee39db9009991e
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2011-06-10 09:13:38 +0000

    Clean up experimental
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1011 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 7c092daefa21a80011f91431808de3e42dc80e25

--- experimental/FLSR/tests/FLSR.R
+++
@@ -1,8 +0,0 @@
-library(odfWeave)
-
-odfWeaveControl(zipCmd = c("C:/Program Files/WinRAR/rar A $$file$$ .", "C:/Program Files/WinRAR/rar X $$file$$"))
-
-iFLSR  <- "C:/Stuff/FLR/help/Sweave/FLSRSweave.odt"
-oFLSR <- "C:/Stuff/FLR/help/text/FLSR.odt"
-
-odfWeave(iFLSR, oFLSR)
