b9c9a794409cfd58104be8c98d5b26a13b0bed82
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2011-07-15 08:22:27 +0000

    Transitional version of FLBD to new pella.tpl
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1098 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 8aa82862d856eb901d85582cd94373f2e6a27ebf

--- pkg/FLBioDym/R/methods.R
+++ pkg/FLBioDym/R/methods.R
@@ -27,69 +27,105 @@ setMethod('residuals', signature(object='FLBioDym'),
 
 # admbBD {{{
 setMethod('admbBD', signature(object='FLBioDym'),
-  function(object, cmdOps=paste("-maxfn 500"), pathNm=getwd(), admbNm="pella") {
+  function(object, cmdOps=paste("-maxfn 500"), dir=tempdir(), admbNm="pellat") {
 
-    pathOrg<-getwd()
-    setwd(pathNm)
-  
-    # admbDatBioDym: writes ADMB input file for model
-    admbDatBioDym<-function(x,file) {
-      ctc <- as.list(drop(model.frame(x[["catch"]])))
-      ctc <- c(nYrs=length(ctc[[1]]),ctc)
+    # Linux
+    if (R.version$os=="linux-gnu") {
+      # executable
+      exe <- paste(system.file("bin", "linux", package="FLBioDym", mustWork=TRUE),
+        admbNm, sep="/")
+      file.copy(exe, dir)
+      # cmd <- paste(paste(dir, admbNm, sep="/"), cmdOps)
+      path <- paste(dir, "/", sep="")
 
-      idx <- as.list(drop(model.frame(x[["index"]])))
-      idx$year <-idx$year[ !is.na(idx$index)]
-      idx$index<-idx$index[!is.na(idx$index)]
+    # Wind0ws
+    } else if (.Platform$OS.type == "windows") {
+      # executable
+      exe <- paste(system.file("bin", "windows", package="FLBioDym", mustWork=TRUE),
+        admbNm, sep="\"")
+      file.copy(exe, dir)
+    }
+    # Mac OSX
+    # or fail!
+    else {
+      stop()
+    }
+
+    # create input files
+    # ctl file
+    ctl <- object@bounds 
+    ctl[,2:4] <- log(ctl[,2:4])
+    ctl <- alply(ctl,1)
+    names(ctl) <- dimnames(object@bounds)$params
+    writeADMB(ctl, paste(path, "/", admbNm, ".ctl", sep=""))
+    
+    # prr file
+    prr <- object@priors 
+    prr <- alply(prr,1)
+    names(prr) <- dimnames(object@priors)$params
+    writeADMB(prr, paste(path, "/", admbNm, ".prr", sep=""))
    
-        res<-c(ctc,c(nYrs=length(idx[[1]]),idx))
-        writeADMB(res,file)
-   
-        return(idx$year)}
-        
-      ctl<-object@bounds 
-      ctl[,2:4]<-log(ctl[,2:4])
-      ctl<-alply(ctl,1)
-      names(ctl)<-dimnames(object@bounds)$params
-      writeADMB(ctl,paste(pathNm,"/",admbNm,".ctl",sep=""))
-       
-      prr<-object@priors 
-      prr<-alply(prr,1)
-      names(prr)<-dimnames(object@priors)$params
-      writeADMB(prr,paste(pathNm,"/",admbNm,".prr",sep=""))
-   
-      params(object)  =propagate(params(   object), seq(dims(object)$iter))
-      
-      dms             =dimnames(object@index)
-      dms$iter        =seq(dims(object)$iter)
-      object@index.hat=FLQuant(NA, dimnames=dms)
-      
-      dms          =dimnames(object@stock)
-      dms$iter     =seq(dims(object)$iter)
-      object@stock =FLQuant(NA, dimnames=dms)
-   
-      fitter<-function(x) {
-        idxYrs<-admbDatBioDym(iter(object,x),paste(pathNm,"/",admbNm,".dat",sep=""))
-          
-        sys.result=system(paste("./", admbNm, " ", cmdOps, sep=""))
-      
-        t1<-read.table(         paste(pathNm,"/",admbNm,".rep",sep=""),skip =18,header=T)
-        t2<-unlist(c(read.table(paste(pathNm,"/",admbNm,".rep",sep=""),nrows=8)))
+    # change wd to avoid ADMB case bug
+    oldwd <- getwd()
+    setwd(path)
+
+    ###
+    browser()
+    # run
+    setADMBBioDym(object, paste(path, admbNm, '.dat', sep=""))
+    system(paste('.', admbNm, sep='/'))
+
+    # call across iters
+    #t.<-m_ply(data.frame(x=seq(dims(object)$iter)),function(x) fitter(x))
+    ###
+
+    # output
+
+    setwd(oldwd)
   
-        object@params[c("r","K","b0","p","q","sigma"),x]<-t2[1:6]
-        #logLik( iter(object,x))<-t2[7]
-        #iter(object,x)@rsdlVar <-t2[8]
-      
-        object@index.hat[,ac(idxYrs),,,,x][]<-unlist(c(t1[t1$Year %in% idxYrs,"IndexFit"]))
-        object@stock[,1:dim(t1)[1],,,,x]    <-unlist(c(t1["Biomass"]))
-        
-        #print(c(sum(t1[,"Index"]*t1[,"Biomass"])/sum(t1[,"Biomass"]^2),
-        #       log((sum((log(t1$Index)-log(t1$IndexFit))^2)/4)^0.5)))
-        
-        object<<-object}
-   
-        t.<-m_ply(data.frame(x=seq(dims(object)$iter)),function(x) fitter(x))
-                
-      setwd(pathOrg)
+    return(object)
+  }
+) # }}}
+
+
+# setADMBBioDym {{{
+setADMBBioDym <- function(object, file) {
   
-      return(object)})
-# }}}
+  #
+  ctc <- as.list(drop(model.frame(object[["catch"]])))
+  ctc <- c(nYrs=length(ctc[[1]]), ctc)
+
+  #
+  idx <- as.list(drop(model.frame(object[["index"]])))
+  idx$year <-idx$year[ !is.na(idx$index)]
+  idx$index<-idx$index[!is.na(idx$index)]
+ 
+  #
+  res <- c(ctc, c(nYrs=length(idx[[1]]), idx))
+  
+  writeADMB(res, file)
+   
+  return(idx$year)
+} # }}}
+
+# runADMBBioDym {{{
+runADMBBioDym <- function(iter, path) {
+
+  # create input .dat file
+  idxYrs <- setADMBBioDym(iter(object, iter),paste(path,admbNm,".dat",sep=""))
+  
+  # run
+  res <- system(paste("./", admbNm, " ", cmdOps, sep=""))
+
+  params <- readADMB(paste(path, admbNm, ".par", sep=""))
+      
+  t1 <- read.table(paste(path, admbNm,".rep",sep=""),skip =18,header=T)
+  t2 <- unlist(c(read.table(paste(path,admbNm,".rep",sep=""),nrows=8)))
+  
+  object@params[c("r","K","b0","p","q","sigma"),x] <- t2[1:6]
+      
+  object@index.hat[,ac(idxYrs),,,,x][] <- unlist(c(t1[t1$Year %in% idxYrs,"IndexFit"]))
+  object@stock[,1:dim(t1)[1],,,,x] <- unlist(c(t1["Biomass"]))
+        
+  object<<-object
+} # }}}
