33e59c38c77761998bc2e138dc3154b3a28c0d65
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2011-07-18 11:46:53 +0000

    refpts methods modified
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: f9526f9071d8974296a6af6e73d23ba0d2077d99

--- branches/FLAdvice_LK_20110610_new/R/refpts.R
+++ branches/FLAdvice_LK_20110610_new/R/refpts.R
@@ -3,55 +3,89 @@
 
 # Copyright 2003-2008 FLR Team. Distributed under the GPL 2 or later
 # Maintainers: Laurence Kell, Cefas & Santiago Cerviño, IEO
-# Last Change: 22 Jan 2010 11:45
 # $Id: refpts.R 888 2011-01-17 00:56:11Z lauriekell $
 
 # constructors  {{{
+# array
+setMethod("refpts", signature(object="array"),
+  function(object, refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'),
+    quantity=c('harvest', 'yield', 'rec', 'ssb', 'biomass', 'revenue', 'cost', 'profit'),
+    iter=1, dimnames=list(refpt=refpt, quantity=quantity, iter=iter), ...) {
 
-# refpts(array)
-setMethod('refpts', signature(object='array'),
-  function(object, refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'), iter=1, ...){
-    # reshape object for iter
-    if(length(dim(object)) < 3)
-      object <- array(object, dim=c(dim(object), iter))
-    else if (dim(object)[3] < iter)
-      object <- array(object, dim=c(dim(object)[-3], iter))
+    # reshape object
+    #  if 2D, object will get 3D with dim[3]=iter
+    if(length(dim(object)) == 2)
+      if(is.null(dimnames(object)))
+        object <- array(c(object), dim=c(dim(object), iter))
+      # keeping dimnames if present
+      else
+        object <- array(c(object), dim=c(dim(object), iter),
+          dimnames=c(dimnames(object), list(iter=1:iter)))
     
-    # add dimnames
-    if(is.null(dimnames(object)))
-    {  
-      # default dnames
-      dmns <- list(
-        refpt=refpt,
-        quantity=c('harvest', 'yield', 'rec', 'ssb', 'biomass', 'revenue', 'cost',
-          'profit')[1:dim(object)[2]],
-        iter=1:dim(object)[3])
-        
+    # dimnames given
+    if(!missing(dimnames))
+      dimnames(object) <- dimnames
+    # or not
+    else {
+      if(!is.null(dimnames(object)))
+        dmns <- dimnames(object)
+      else
+        dmns <- list(refpt=refpt[1:dim(object)[1]],
+          quantity=quantity[1:dim(object)[2]],
+          iter=1:dim(object)[3])
+      if (!missing(refpt))
+        dmns$refpt <- refpt
+      if (!missing(quantity))
+        dmns$quantity <- quantity
+      if (!missing(iter))
+        dmns$iter <- iter
       dimnames(object) <- dmns
-      }
+    }
 
+    return(new('refpts', object))
+  }
+)
 
-     return(new('refpts', object))})
-     
-setMethod('refpts', signature(object='missing'),
-  function(refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'), iter=1, ...){
-    if (length(iter) == 1 && ac(iter) == '1')
-       iter <- seq(length=as.numeric(iter))
-      
-    refpts(array(as.numeric(NA), dim=c(length(refpt), 8, length(iter))),refpt=refpt, iter=iter, ...)})
+# vector
+setMethod("refpts", signature(object="vector"),
+  function(object, refpt="missing", quantity="missing", iter="missing",
+    dimnames="missing", ...) {
+    
+    # get dims from args
+    if(!missing(dimnames))
+      object <- array(object, dimnames=dimnames, dim=unlist(lapply(dimnames, length)))
+    else {
+      dmns <- dimnames(new("refpts"))
+      if(!missing(refpt))
+        dmns$refpt <- refpt
+      if(!missing(quantity))
+        dmns$quantity <- quantity
+      if(!missing(iter))
+        dmns$iter <- iter
+      object <- array(object, dimnames=dmns, dim=unlist(lapply(dmns, length)))
+    }
 
-setMethod('refpts', signature(object='numeric'),
-  function(object, refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'), iter=1, ...)
-    refpts(array(as.numeric(object), dim=c(length(refpt), 8, iter)), refpt=refpt,iter=iter, ...))
+    return(refpts(object, ...))
+  }
+)
 
-setMethod('refpts', signature(object='logical'),
-  function(object, refpt=c('f0.1', 'fmax', 'spr.30', 'msy', 'mey'), iter=1, ...)
-    refpts(array(as.numeric(object), dim=c(length(refpt), 8, iter)),refpt=refpt,iter=iter, ...))
+# missing
+setMethod("refpts", signature(object="missing"),
+  function(object, ...) {
+    return(refpts(as.numeric(NA), ...))
+  }
+)
 
-setMethod('refpts', signature(object='refpts'),
-  function(object, ...)
-    refpts(object@.Data, refpt=dimnames(object)$refpt,...))
+# logical
+setMethod("refpts", signature(object="logical"),
+  function(object, ...) {
+    return(refpts(as.numeric(object), ...))
+  }
+)
 
+# }}}
+
+# show {{{
 setMethod('show', signature(object='refpts'),
   function(object){
 		cat("An object of class \"refpts\":\n")
@@ -67,21 +101,28 @@ setMethod('show', signature(object='refpts'),
 
 		if(dim(object)[3] != 1)
 			cat("iters: ", dim(object)[3],"\n\n")
-    })
+  }
+) # }}}
 
+# propagate {{{
 setMethod('propagate', signature(object='refpts'),
   function(object, iter, fill.iter=TRUE){
   
     res <- refpts(object, iter=iter)
     if(fill.iter== FALSE)
       res[,,2:iter] <- as.numeric(NA)
-    return(res)})
+    return(res)
+  }
+) # }}}
 
+# refpts<- {{{
 setMethod('refpts<-', signature(object='FLBRP', value='refpts'),
   function(object, value){
     slot(object, 'refpts') <- value
 
-    return(object)})
+    return(object)
+  }
+)
     
 setMethod('refpts<-', signature(object='FLBRP', value='numeric'),
   function(object, ..., value){
@@ -101,6 +142,7 @@ setMethod('refpts<-', signature(object='FLBRP', value='numeric'),
       refpts(object)[] <- value}
       
     return(object)})
+# }}}
 
 setMethod('refpts', signature(object='FLBRP'),
   function(object, ...){
