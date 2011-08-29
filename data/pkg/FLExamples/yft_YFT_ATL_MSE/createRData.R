53d88a9b7d4c00ec9c31842875afda968eb8bc15
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2011-08-29 09:22:14 +0000

    Reorganized data tree. Data files for FLExamples now inside pkg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1181 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: c476d9c42924edf463cfaf06b8da2f3babdeb8ba

---
+++ data/pkg/FLExamples/yft_YFT_ATL_MSE/createRData.R
@@ -0,0 +1,19 @@
+# .R - 
+# /R/.R
+
+# Copyright 2003-2011 FLR Team. Distributed under the GPL 2 or later
+# $Id:  $
+
+library(FLCore) 
+library(ggplotFL) 
+
+runs <- paste("S", 0:12, sep="")
+
+yft <- FLStocks(mlply(runs, function(x) readVPA2Box(paste(x, 'yft2008.ctl', sep="/"), m=0.8)))
+
+names(yft) <- paste("Scenario", 0:12, sep=" ")
+attributes(yft) <- attributes(yft)[1:4]
+attr(yft, 'lock') <- TRUE
+attr(yft, 'desc') <- paste('YFT ATL SA runs. Created on', date(), ".",  R.Version()$version.string)
+
+save(yft, file='yft.RData')
