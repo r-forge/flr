9243edaa14d14bd7518b20bb30b718a392fa3380
lauriekell (Dr ) <laurieKell@gmail.com>
2011-09-16 06:56:41 +0000

    hcr
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1251 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 8f36ff160bdc4be125667a10b502be35dbbcdc56

---
+++ pkg/FLBioDym/R/hcr.R
@@ -0,0 +1,31 @@
+
+setGeneric('hcr', function(object, ...) standardGeneric('hcr'))
+setMethod( 'hcr', signature(object='FLBioDym'),
+  function(object,Ftar=0.8,Btrig=0.75,Fmin=0.025,Blim=0.25,...){
+      ## Reference Points
+      Btrig=refpts(object)["bmsy"]*Btrig
+      Blim =refpts(object)["bmsy"]*Blim
+      Fmin =refpts(object)["fmsy"]*Fmin
+      Ftar =refpts(object)["fmsy"]*Ftar
+  
+      ## HCR
+      if (Blim>=Btrig) stop("Btrig must be greater than Blim")
+      a= FLPar((Ftar-Fmin)/(Btrig-Blim))
+      b= FLPar(Ftar-a*Btrig)
+      
+      ## Calc F
+      SSB =stock(object)[,dims(object)$year]
+      val =qmax(qmin(sweep(sweep(SSB,6,a,"*"),6,b,"+"),Ftar),Fmin)
+  
+      return(val)})
+
+
+setGeneric('TAC', function(object, ...) standardGeneric('TAC'))
+setMethod( 'TAC', signature(object='FLBioDym'),
+  function(object,harvest,...){
+  ## gets TAC
+  yr    =dims(object)$year
+  object=fwd(window(object,end=yr), harvest=harvest)
+
+  return(catch(object)[,ac(yr)])})
+
