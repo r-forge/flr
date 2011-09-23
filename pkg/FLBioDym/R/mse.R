5d0a5bc83e0aa4ba4f4cead7e45bbd6462b60501
lauriekell (Dr ) <laurieKell@gmail.com>
2011-09-23 07:24:02 +0000

    mse func
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1268 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 4abc902fff266cebbb6c499acbecbeff62563ee3

---
+++ pkg/FLBioDym/R/mse.R
@@ -0,0 +1,48 @@
+
+mseFLBioDym<-function(OM,start,sr,srRsdl,
+                    CV   =0.3,
+                    Ftar =0.75,    Btrig=0.75,
+                    Fmin =Ftar*0.1,Blim =Btrig*0.0,
+                    Bpct =0.5,     Fpct =0.5,
+                    jk    =FALSE,
+                    bounds=NULL){
+  
+  ## Get number of iterations in OM
+  nits  =dims(OM)$iter
+
+  #### Observation Error (OEM) setup #######################
+  ## Random variation for Catch & CPUE, CV=0.25%
+  bd       =as(OM,"FLBioDym")
+  index(bd)=index(bd)*rlnorm(prod(dim(index(bd))),0,CV)
+  
+  if (!is.null(bounds)) bd@bounds=bounds
+    
+  ## Loop round years
+  for (iYr in start:(range(OM,"maxyear")-2)){
+  #iYr = (start:(range(OM,"maxyear")-2))[1]
+     cat("===================", iYr, "===================\n")
+     ## add year to FLBD
+     bd                 =window(bd,end=iYr)
+     index(bd)[,ac(iYr)]=stock(OM)[,ac(iYr)]*rlnorm(prod(dim(index(bd)[,ac(iYr)])),0,CV)
+     catch(bd)[,ac(iYr)]=computeCatch(OM)[,ac(iYr)]
+ 
+     if (jk){
+      hv =hcrJK(bd,Ftar,Btrig,Fmin,Blim,Fpct,Bpct) 
+     }else{ 
+       bd =admbBD(bd)
+       hv =hcr(bd,Ftar,Btrig,Fmin,Blim) 
+       }
+     
+     TAC=TAC(bd,hv)
+       
+     #### Now you have TAC take it from OM
+     ctrl    <-fwdControl(data.frame(year=iYr+2,max=c(NA,2),quantity=c("catch","f")))
+     dms     <-dimnames(ctrl@trgtArray)
+     dms$iter<-1:nits
+     ctrl@trgtArray<-array(NA,lapply(dms,length),dms)
+     ctrl@trgtArray[1,"val", ]<-TAC
+     ctrl@trgtArray[2,"max", ]<-0.35
+
+     OM <-fwd(OM,ctrl=ctrl,sr=sr,sr.residuals=srRsdl)}
+
+   return(OM)}
