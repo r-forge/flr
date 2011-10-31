d87fe442828ea24e4da0d2549029ba33ef7c49db
lauriekell (Dr ) <laurieKell@gmail.com>
2011-10-31 09:37:21 +0000

    fwd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1351 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 259e54522f8b22860f62c7ac58b5703fe767becf

---
+++ pkg/FLAdvice/R/fwd.R
@@ -0,0 +1,12 @@
+
+setMethod("fwd", signature(object="FLBRP", ctrl="missing"),
+    function(object,ctrl,
+               sr=object, sr.residuals=FLQuant(1,dimnames=dimnames(rec(object))), sr.residuals.mult=TRUE,
+               availability=NULL,maxF=2.0)
+    {                          
+    ctrl=fbar(object)[,-1]  
+    stk=as(object,"FLStock") 
+    res=fwd(stk,f=ctrl,
+                   sr=sr,sr.residuals=sr.residuals,sr.residuals.mult=sr.residuals.mult,availability=availability,maxF=maxF)
+                      
+    return(res)})
