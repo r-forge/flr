8f952fe6cd9819d6410dd47de8f31359f6f9fbe4
imosqueira (Dr ) <iago.mosqueira@gmail.com>
2012-02-10 10:57:49 +0000

    Cleaned up lh.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1541 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: ab9df6dfc22faeb646ff786f13317dcb539b43b2

--- pkg/FLAdvice/R/lh.R
+++ pkg/FLAdvice/R/lh.R
@@ -1,54 +1,75 @@
-gislasim=function(par,sl=5,sr=5000){
+# lh.R - Life history-based population generators
+# FLAdvice/R/lh.R
+
+# Copyright 2003-2012 FLR Team. Distributed under the GPL 2 or later
+# Maintainer: Laurie Kell, ICCAT
+# $Id: $
+
+# gislasim {{{
+# Completes a parameter vector from L_{inf}
+
+gislasim <- function(par, sl=5, sr=5000) {
    
+  # make names lower case
   names(dimnames(par))=tolower(names(dimnames(par)))
 
-  ## growth parameters
-  if (!("t0" %in% dimnames(par)$params)) par=rbind(par,FLPar("t0"=0))
-  if (!("a"  %in% dimnames(par)$params)) par=rbind(par,FLPar("a" =0.01))
-  if (!("b"  %in% dimnames(par)$params)) par=rbind(par,FLPar("b" =3))
-    
-  if (!("k"  %in% dimnames(par)$params)) par=rbind(par,FLPar("k"=exp(0.5236+c(log(par["linf"]))*-0.4540)))
+  # growth parameters
+  if (!("t0" %in% dimnames(par)$params))
+    par=rbind(par,FLPar("t0"=0))
+  if (!("a"  %in% dimnames(par)$params))
+    par=rbind(par,FLPar("a" =0.01))
+  if (!("b"  %in% dimnames(par)$params))
+    par=rbind(par,FLPar("b" =3))
+  if (!("k"  %in% dimnames(par)$params))
+    par=rbind(par,FLPar("k"=exp(0.5236+c(log(par["linf"]))*-0.4540)))
  
-  ### why does rbind(FLPar,numeric) not return an FLPar
-  #par=rbind(par,FLPar("sinf"=par["a"]*par["linf"]^par["b"]))
-
-  ## maturity parameters
-  par=FLPar(rbind(par,FLPar(c("a50"=exp(0.8776*log(par["linf",])-0.038),"ato95"=0,"asym"=1.0))))
+  # maturity parameters
+  par=FLPar(rbind(par,
+    FLPar(c("a50"=exp(0.8776*log(par["linf",])-0.038),"ato95"=0,"asym"=1.0))))
   par["a50"]=invVonB(par,c(par["a50"]))
   
   ## selectivity
-  par=rbind(par,FLPar(a1=par["a50"],sl=sl,sr=sr))
+  par=rbind(par, FLPar(a1=par["a50"],sl=sl,sr=sr))
   
-  return(par)}
+  return(par)
+} # }}}
 
-#### Life Hisotory Generator ####################################################
-# TODO lh(gislasim(par))
-# TODO Add support for FLSR
-lh=function(par,
-            growth       =vonB,
-            fnM          =function(par,len,T=290,a=FLPar(c(-2.1104327,-1.7023068,1.5067827,0.9664798,763.5074169)))
-                                    exp(a[1]+a[2]*log(len) + a[3]*log(par["linf"]) + a[4]*log(par["k"]) + a[5]/T),
-#            mFn          =function(par,len) exp(0.55 - 1.61*log(len) + 1.44*log(par["linf"]) + log(par["k"])),
-            fnMat        =logistic,
-            selFn        =dnormal,
-            sr           =list(model="bevholt",steepness=0.9,vbiomass=1e3),
-            age=1:40+0.5,T=290,...){
+# lh: Life history population generator{{{
+lh <- function(par, growth=vonB,
+  fnM=function(par, len, T=290,
+    a=FLPar(c(-2.1104327,-1.7023068,1.5067827,0.9664798,763.5074169)))
+    exp(a[1]+a[2]*log(len) + a[3]*log(par["linf"]) + a[4]*log(par["k"]) + a[5]/T),    
+  fnMat=logistic, selFn=dnormal,
+  sr=list(model="bevholt",steepness=0.9,vbiomass=1e3),
+  age=1:40+0.5, T=290, ...) {
+
+
+  # age object
+  age <- FLQuant(age,dimnames=list(age=floor(age)))
+
+  # length
+  len <- growth(par[c("linf","t0","k")],age)
   
-   age=FLQuant(age,dimnames=list(age=floor(age)))
-   len=growth(par[c("linf","t0","k")],age)
-   wts=par["a"]*len^par["b"]       / 1000
+  # weights
+  wts <- par["a"]*len^par["b"] / 1000
 
-   m.   =fnM(  par=par,len=len,T=T)
-   mat. =fnMat(par,age)
-   sel. =selFn(par,age)
+  # M
+  m. <- fnM(par=par, len=len, T=T)
+  
+  # maturity
+  mat. <- fnMat(par, age)
 
-   ## create a FLBRP object to   calculate expected equilibrium values and ref pts
-   dms=dimnames(m.)
-   res=FLBRP(stock.wt       =wts,
-             landings.wt    =wts,
-             discards.wt    =wts,
-             bycatch.wt     =wts,
-             m              =m.,
+  # selectivity
+  sel. <- selFn(par, age)
+
+  # create a FLBRP object to calculate expected equilibrium values and refpts
+  dms <- dimnames(m.)
+  
+  res <- FLBRP(stock.wt=wts,
+             landings.wt=wts,
+             discards.wt=wts,
+             bycatch.wt=wts,
+             m=m.,
              mat            =FLQuant(mat., dimnames=dimnames(m.)),
              landings.sel   =FLQuant(sel., dimnames=dimnames(m.)),
              discards.sel   =FLQuant(0,    dimnames=dimnames(m.)),
@@ -57,21 +78,23 @@ lh=function(par,
              m.spwn         =FLQuant(0,    dimnames=dimnames(m.)),
              availability   =FLQuant(1,    dimnames=dimnames(m.)))
 
-  # Set wt units
+  # set wt units
   wtSlots <- c("stock.wt", "landings.wt", "discards.wt", "bycatch.wt")
+
   for (wtSlot in wtSlots)
     units(slot(res,wtSlot)) <- "kg"
 
+  # FApex
+  range(res, c("minfbar","maxfbar"))[] <- as.numeric(dimnames(landings.sel(
+    res)[landings.sel(res)==max(landings.sel(res))][1])$age)
 
-   ## FApex
-   range(res,c("minfbar","maxfbar"))[]<-as.numeric(dimnames(landings.sel(res)[landings.sel(res)==max(landings.sel(res))][1])$age)
-
-   ## replace any slot passed in as an arg
+   # replace any slot passed in as an arg
    args<-list(...)
+
    for (slt in names(args)[names(args) %in% names(getSlots("FLBRP"))[names(getSlots("FLBRP"))!="fbar"]])
      slot(res, slt)<-args[[slt]]
 
-   ## Stock recruitment relationship
+   # Stock recruitment relationship
    model(res) =do.call(sr$model,list())$model
    params(res)=FLPar(abPars(sr$model,spr0=spr0(res),s=sr$steepness,v=sr$vbiomass))
 
@@ -86,6 +109,5 @@ lh=function(par,
   
    return(brp(res))}
 
-#ex=lh(gislasim(FLPar(linf=70)))
-#ggplot(ex[[c("m","stock.wt","mat","landings.sel")]])+geom_line(aes(age,data))+facet_wrap(~qname,scale="free")
-#plot(ex)
+
+# }}}
