158f60747ca854874778c9a09392c275975482db
lauriekell (Dr ) <laurieKell@gmail.com>
2012-04-02 06:47:32 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1609 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    Former-commit-id: 301958cd2ffe1b0c42613a7750df3363a6220b5a

--- pkg/FLAdvice/R/leslie.R
+++ pkg/FLAdvice/R/leslie.R
@@ -2,7 +2,7 @@ setGeneric('leslie', function(object, ...)
   	standardGeneric('leslie'))
 
 setMethod("leslie", signature(object="FLBRP"),
-  function(object,fbar=FLQuant(0),...){
+  function(object,fbar=FLQuant(0),numbers=TRUE,...){
 
   args=list(...)  
   for (slt in names(args)[names(args) %in% names(getSlots("FLBRP"))])
@@ -16,12 +16,17 @@ setMethod("leslie", signature(object="FLBRP"),
   diag(mx[-1,-length(ages)])   =exp(-m(object)[-length(ages)])
   if (range(object)["plusgroup"]==range(object)["max"])
     mx[length(ages),length(ages)]=exp(-m(object)[length(ages)])
-  print(fbar(object))
   
   #recruitment
-  tmp =  mat(object)*stock.wt(object)*stock.n(object)[,1]
-  tmp2=  apply(tmp,2:6,sum)
-  mx[1,]=rec(object)[1]%*%tmp%/%tmp2%/%stock.n(object)[,1]
+  tmp   = mat(object)*stock.wt(object)*stock.n(object)[,1]
+  tmp2  = apply(tmp,2:6,sum)
+  mx[1,]= rec(object)[1]%*%tmp%/%tmp2%/%stock.n(object)[,1]
+  
+  if (!numbers){
+       diag(mx[-1,-length(ages)])
+       print(c(stock.wt(object)[-1,1])/c(stock.wt(object)[-length(ages),1]))
+     mx[1,]=c(stock.wt(object)[,1])*mx[1,]
+     }
   
   return(mx)})
 
