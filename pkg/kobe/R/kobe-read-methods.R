f90d7be8eafc15137ea9cf50f8af39e8a5888ef4
lauriekell (Dr ) <laurieKell@gmail.com>
2012-12-15 11:13:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1798 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    Former-commit-id: b3f758bacfc36427f52e6c25e14c201a514b1412

---
+++ pkg/kobe/R/kobe-read-methods.R
@@ -0,0 +1,33 @@
+utils::globalVariables(c("sims","ptsYrs"))
+
+setMethod('kobe',  signature(file='character',method="character"), function(file,method=c("aspic","adapt","bsp","mfcl","ss","sam","vpa"),
+                                                                            dir="",what=c("sims","trks","pts","smry","wrms")[1],
+                                                                            prob=c(0.75,0.5,0.25),ptYrs=NULL,nwrms=10,...) {
+   
+    method=tolower(method)
+    if (any("2box" == method)) method["2box" == method]="adapt"   
+    switch(substr(method[1],1,2),
+           ad=kobe2box( file,dir=dir,what=what,prob=prob,ptYrs=ptYrs,nwrms=nwrms,...),
+           as=kobeAspic(file,dir=dir,what=what,prob=prob,ptYrs=ptYrs,nwrms=nwrms,...),
+           mf=kobeMFCL( file,dir=dir,what=what,prob=prob,ptYrs=ptYrs,nwrms=nwrms,...),
+           ss=kobeSS3(  file,dir=dir,what=what,prob=prob,ptYrs=ptYrs,nwrms=nwrms,...))
+    })
+
+
+setMethod('kobe',  signature(file="data.frame",method="missing"),  function(file,method,what=c("sims","trks","pts","smry","wrms")[1],prob=c(0.75,0.5,.25),ptYrs=NULL,nwrms=10){
+  
+  res=llply(object, function(x,what=what,prob=prob,ptYrs=ptYrs,nwrms=nwrms)
+    kobeFn(object,what=what,prob=prob,ptYrs=ptYrs,nwrms=nwrms),
+            what=what,prob=prob,ptYrs=ptYrs,nwrms=nwrms)
+  
+  res=list(trks=ldply(res, function(x) x$trks),
+           pts =ldply(res, function(x) x$pts),
+           smry=ldply(res, function(x) x$smry),
+           wrms=ldply(res, function(x) x$wrms),
+           sims=ldply(res, function(x) x$sims))
+  
+  if (length(what)==1)
+    return(res[[what]])
+  else
+    return(res[what]) })
+
