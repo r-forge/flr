2d8e45c9dad45d5e2c3426a4606f0443d924f4e5
lauriekell (Dr ) <laurieKell@gmail.com>
2013-02-24 11:01:10 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/flr@1862 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    Former-commit-id: 3b0b89036a1988772a8d11235abf35712107e4eb

--- pkg/aspic/R/aspic-iter.R
+++ pkg/aspic/R/aspic-iter.R
@@ -1,14 +1,20 @@
 if (!isGeneric("iter")) setGeneric("iter", function(obj, ...)
   standardGeneric("iter"))
 
-setMethod("iter", signature(obj="FLComp"),
+setMethod("iter", signature(obj="aspic"),
           function(obj, iter) {
             
-          obj@params=iter(obj@params,  iter)
-          obj@control=iter(obj@control,iter)
-          obj@catch =iter(obj@catch,   iter)
-          obj@stock =iter(obj@stock,   iter)
-          
+            # copy the iterate into the new slots
+            names. <- c(getSlotNamesClass(obj, 'FLArray'),getSlotNamesClass(obj, 'FLPar'))
+            for(s. in names.)
+            {
+              if(dims(slot(obj, s.))$iter == 1)
+                slot(obj, s.) <- iter(slot(obj, s.), 1)
+              else
+                slot(obj, s.) <- iter(slot(obj, s.), iter)
+            }
+            
+            
           if ("iter" %in% names(obj@index)){
           flag=obj@index$iter %in% iter
           obj@index=obj@index[flag,]}
